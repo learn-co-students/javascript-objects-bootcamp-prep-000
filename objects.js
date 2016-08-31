3f2922826954d7197aaadce14c2aeebafa84537f
LaDerrius Williams <mrldaniellewilliams@gmail.com>
2016-08-31 17:27:15 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,7 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
-  return playlist;
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
+  return playlist
 }
