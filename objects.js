5fcf56bd7e05ba40dfa5e596d56c4032c9846182
Ivan Collado <collado.ip@gmail.com>
2016-09-06 06:36:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+playlist = {artist1: 'song'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  playlist = {}
+  return playlist
+}
