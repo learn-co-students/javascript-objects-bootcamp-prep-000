cd2e7888323179a08936db3a88ede3ebcdb6fd7e
jagneesh malhotra <j.g.malhotra@gmail.com>
2016-09-10 21:22:38 +0000

    Done.

--- objects.js
+++ objects.js
@@ -11,5 +11,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
 
-  return playlist;
+  playlist[artistName]
 }
