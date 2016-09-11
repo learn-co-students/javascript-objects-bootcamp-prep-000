de9d0fa85794ac71f33b0f375142fb5d2c193056
Fernando Gomez <fhergomez37@gmail.com>
2016-09-11 19:51:57 +0000

    Done.

--- objects.js
+++ objects.js
@@ -10,5 +10,6 @@ function updatePlaylist(playlist,artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist,artistName){
-  return delete playlist.artistName;
+  delete playlist[artistName];
+  return playlist;
 }
