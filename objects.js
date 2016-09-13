7d02e5fd5a3620ab6c6f0e85602fe9dcc1f97153
Alexis Hovey <alexis.hovey@gmail.com>
2016-09-13 19:15:36 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,6 @@
+var playlist = new Object({TaylorSwift: "Blank Space"});
+
+function updatePlaylist(playlist, "Phil Ochs", "Here's To The State of Mississippi")
+playlist["Phil Ochs"] = "Here's To The State of Mississippi";
+  return playlist
+  }
