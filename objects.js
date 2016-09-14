ab97d1e7ffb30fe88437d7833bf7d5a89f45a939
Justin Mayerick <jmayeric@gmail.com>
2016-09-14 12:36:08 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Vibrasphere: "Tierra Azul"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
