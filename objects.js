39a5cdad85390770686475b36ac84d01e8d9737e
Boris Boroda <bboroda@yahoo.com>
2016-09-17 01:26:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {};
+playlist['madonna'] = 'like a virgin';
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
