234d6b41f4d3d3b210757914b8cfa5da228dc416
Keith Hubert <keith.hubert@gmail.com>
2016-09-20 02:10:17 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,10 @@
-var playlist = new Object({artistName: `songTitle`})
+var playlist = {artistName: `songTitle`}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
-  return {}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
 }
