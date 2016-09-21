1ebcac39d01dce1fcb8cea2833e1751a68fd9c16
Brad White <white.bradleyj@gmail.com>
2016-09-21 01:41:07 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  artistName: "songTitle"
+};
+
+var updatePlaylist = function(playlist,artistName,songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+var removeFromPlaylist = function(playlist, artistName){
+  delete playlist[artistName];
+  return playlist
+}
