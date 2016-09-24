a93a91ccf57efc0eebce07df0935c00970939b2b
Luke O'Connor <luke.p.oconnor@gmail.com>
2016-09-24 05:55:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {drake: 'energy'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
