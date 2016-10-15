9fb667f85ce6514f901e004cd11821c54b63fd88
Rachel Whaley <rachelwhaley24@gmail.com>
2016-10-15 16:43:18 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+//var playlist = {};
+var playlist = new Object();
+
+playlist["death cab"] = "Marching Band of Manhattan"
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
