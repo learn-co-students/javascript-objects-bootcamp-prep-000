584e0c3797d168a92eff2d2d97e0240c4c221b90
Vanessa Jimenez <vjimenez127@gmail.com>
2016-10-24 00:41:13 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  Mana: "Mariposa",
+  Darius: "Hot Hands"
+};
+
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+
+  return playlist;
+}
