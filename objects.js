92a7216d939046e2b244845d1522ab72f65073f0
Azekah Griffiths <azekah.griffiths@icloud.com>
2016-10-25 18:55:23 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,12 @@
-var playlist = {Tessanne:'Who'}
+var playlist = { artistName: 'songTitle'}
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle
-  return playlist
+function updatePlaylist(playlist,artistName,songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
-function removeFromPlaylist(playlist, artistName) {
+function removeFromPlaylist(playlist,artistName)
+{
   delete playlist[artistName];
-  return playlist
+  return playlist;
 }
