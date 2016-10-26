f90c46ff1f9fe11832cb1a02660099d74c10c342
Joe Browm <jaybrizle@gmail.com>
2016-10-26 23:13:59 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist ={
+  name: ''
+}
+
+function updatePlaylist(playlist, name, song) {
+  playlist[name] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist[name]
+  return playlist
+}
