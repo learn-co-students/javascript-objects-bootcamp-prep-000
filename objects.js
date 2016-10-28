d10a9f93074635490a6eb75bf8d331d5607d3d12
jason brown <j.grant.brown@gmail.com>
2016-10-28 19:45:40 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlayList(playlist,artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist,artistName){
-  delete playlist.artistName;
-  return playlist
+  return delete playlist.artistName;
+
 }
