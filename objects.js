46e447f0e302389f96e175414719cece56f63c66
jason brown <j.grant.brown@gmail.com>
2016-10-28 19:31:23 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistName: "songTitle"};
 
 function updatePlayList(playlist,artistName,songTitle){
-  Object.assign({artistName:"songTitle"}})
+  Object.assign({artistName:"songTitle"});
   return playlist
 }
