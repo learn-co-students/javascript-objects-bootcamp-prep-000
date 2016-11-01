75001a3b0d49165fc1b62b2ddf80ef81789321b7
Xuebing Wang <xbwang93@gmail.com>
2016-11-01 01:00:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {artistName: 'songTitle'}
+
+function updatePlaylist (obj, artistName, songTitle){
+  obj[artistName] = songTitle
+
+  return obj
+}
+
+function removeFromPlaylist (obj, artistName) {
+
+  delete obj[artistName]
+  return obj
+
+}
