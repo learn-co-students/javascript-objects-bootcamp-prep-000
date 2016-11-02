ff8f47beee0d3bb160b7ae7f4f85059eb22ae966
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-02 03:15:28 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  'David Bowie': 'Changes',
+  Beyonce: 'Six Inch'
+}
+
+function updatePlaylist(playlist, artist, song title) {
+  playlist['David Bowie']=Changes
+  return playlist
+}
