44e271f81a81ca0b26c6dde7b931c8ff56e366e5
Jackson Hoang <jkhoang313@gmail.com>
2016-11-03 05:00:31 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Kaskade: "Never Sleep Alone"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
