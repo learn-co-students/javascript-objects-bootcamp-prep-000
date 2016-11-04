50d39f052b102d835b56a0a5dc2d9e8a58c014a2
Paolo Veloce <pablo.veloce@gmail.com>
2016-11-04 19:48:24 +0000

    Done.

--- objects.js
+++ objects.js
@@ -9,3 +9,14 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist
 }
+
+/* EXPLANATION DIFFERENCE BETWEEN OBJ.PARAMETER AND OBJ[PARAMETER]
+playlist.artistName <- This is looking for a key in the playlist object called
+artistName while this
+playlist[artistName] is looking for a key in the playlist object using the variable
+artistName
+
+So basically this playlist.artistName is equivalent to doing this playlist["artistName"]
+
+blockquote by NICHOLAS STEPHENSON https://learn.co/NStephenson
+*/
