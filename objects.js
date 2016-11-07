682302240e8c18fe201d188f1c10073f563357a9
Lauren Albritton <dancerlma@yahoo.com>
2016-11-07 15:46:58 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {AWOLNATION: 'Sail', Kaney: "Gold Digger"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye;
+
+  return playlist;
+}
