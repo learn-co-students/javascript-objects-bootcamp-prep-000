c93e8d6b5908f3e940b1f8433498e6ef7ee3deed
Clark moss <cmoss@mail2business.com>
2016-11-17 20:35:03 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,17 +1,17 @@
 
-var playlist = {artist_names: "song titles"};
+var playlist = {artist: "song titles"};
 
 
 function updatePlaylist(playlist, artistName, songTitle){
 var artistName = 'Phil Ochs'
 var songTitle = "Here's to the State of Mississippi"
-playlist["artistName"] ="songTitle"
+playlist["Phil Ochs"] ="Here's to the State of Mississippi"
 return playlist
 
 }
 
 function removeFromPlaylist (playlist, artistName){
-var playlist = {artistName:"Kanye"}}
+var playlist = {}
 delete playlist.Kanye;
 
 return playlist
