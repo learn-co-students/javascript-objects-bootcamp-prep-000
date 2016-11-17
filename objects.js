08c58c32fb488e81ef2020b23b44f00d972aae3d
Clark moss <cmoss@mail2business.com>
2016-11-17 19:42:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,18 @@
+
+var playlist = {artist_names: "song titles"};
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+var artistName = 'Phil Ochs'
+var songTitle = "Here's to the State of Mississippi"
+playlist["artistName"] ="songTitle"
+return playlist
+
+}
+
+function removeFromPlaylist (playlist, artistName){
+var playlist = {artistName:"Kanye"}}
+delete playlist.Kanye;
+
+return playlist
+}
