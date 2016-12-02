5319cb7daf6b3649267d01821fbfaf5c81941713
Molly Gelsey <mag242@gmail.com>
2016-12-02 21:24:51 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {'Drake': 'Fake Love'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+
+  return Object.assign(playlist, { [artistName]: songTitle })
+
+}
+
+function removeFromPlaylist(playlist, artistName){
+   delete playlist[artistName];
+   return playlist
+
+}
