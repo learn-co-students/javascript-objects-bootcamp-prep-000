97ec7ca889deeab39c1b9e7180634459f4e2b1c4
Kevin Adkins <kevinladkins@gmail.com>
2016-12-02 19:01:46 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Bauhaus : "Kick in the Eye",
+  TheCure : "Hot Hot Hot",
+  JoyDivision : "Love Will Tear Us Apart"
+}
+
+funtion updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({} playlist, {[key] : value})
+}
