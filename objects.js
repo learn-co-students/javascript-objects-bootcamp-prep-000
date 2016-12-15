d52a0e137dd1f87402c68b0fbaf4b4995614be5d
Siobhan Martin <bklyngal1994@gmail.com>
2016-12-15 18:54:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {Solange : "Cranes in the sky"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
