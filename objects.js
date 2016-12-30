090fdd919361a24ca1db954858830c00d60b678e
Ryan Christoffersen <ryanelliotchristo@gmail.com>
2016-12-30 02:05:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  Chromatics: "Cherry"
+};
+
+function updatePlaylist(playlist, artist, song) {
+  return Object.assign(playlist, {[artist]: song});
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
