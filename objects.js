1d7c93486af3438dd6f54306008b9a5a07d18798
Grant MacManus <flatiron@macdav.net>
2016-12-31 17:09:09 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'Rachmaninoff': 'Piano Sonata 3'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
