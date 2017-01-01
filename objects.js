3348285f4a19b583c90d657fe89612774ab65e19
mehdi kalantari <kalan.mahdi@yahoo.com>
2017-01-01 04:35:53 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {};
+var playlist = { sadegh: "zohr" };
+
+function updatePlaylist(playlist, artistName, songTitle)
+{
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName)
+{
+  delete playlist[artistName];
+  return playlist;
+}
