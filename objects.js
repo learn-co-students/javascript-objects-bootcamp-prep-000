e4eb9ff6dec2a89d1bce4aa600f864e34e628126
Elvis Delahoz <elvisdelahoz@gmail.com>
2017-01-04 21:02:44 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
-var playlist = {Bono: 'One', Madonna: 'Holiday' }
+var playlist = {Bono: "One" };
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist = [Phil Ochs: "Here's to the State of Mississippi"];
+  return playlist;
+}
+updatePlaylist(playlist, artistName, songTitle); /*
+function removeFromPlaylist(playlist, artistName) {
+  playlist = [Phil Ochs: "Here's to the State of Mississippi"];
+  return playlist;
+  }
+removeFromPlaylist(playlist, artistName);
+*/
