bfde28cc9ad1af0b0668cc1e85831910bba339a2
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-01-08 19:42:43 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+playlist = {}
+Object.assign(playlist, {artist: 'song title'})
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
