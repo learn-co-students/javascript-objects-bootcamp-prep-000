14116103c40b83bac09bbad50a5fe55fd8d0f36e
Elvis Delahoz <elvisdelahoz@gmail.com>
2017-01-09 01:51:46 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,12 +1,14 @@
 var playlist = {Bono: "One" };
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist = [Phil Ochs: "Here's to the State of Mississippi"];
+//  playlist = [Phil Ochs: "Here's to the State of Mississippi"];
+  Object.assign(playlist, { [artistName]: songTitle });
   return playlist;
 }
-updatePlaylist(playlist, artistName, songTitle); /*
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
 function removeFromPlaylist(playlist, artistName) {
-  playlist = [Phil Ochs: "Here's to the State of Mississippi"];
+  Object.assign(playlist, { Kanye: "Gold Digger" });
+//  console.log(playlist); just wanted to see the output.
+  delete playlist.Kanye;
   return playlist;
   }
-removeFromPlaylist(playlist, artistName);
-*/
+removeFromPlaylist(playlist, 'Kanye');
