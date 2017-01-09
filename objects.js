85fc62312545ecc9b10e20d291f5ee5f8fc3f323
Assina Kahamba <assinam@gmail.com>
2017-01-09 17:11:12 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
-var playlist = new Object();
+var playlist = {
+  akon: 'O Africa',
+
+}
+
 
 function updatePlaylist(obj, artist, song){
   obj[artist] = song;
@@ -6,7 +10,13 @@ function updatePlaylist(obj, artist, song){
 return obj;
 }
 
-function removeFromPlaylist(obj, key){
+function removeFromPlaylist(playlist, artistName){
 
-  delete obj.key;
+  delete playlist.artistName;
+playlist = {
+
+
+  }
+
+ return playlist;
 }
