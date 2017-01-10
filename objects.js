c2ce94f0346ce6a5018ef178682b5063a7e95164
John Knotts <jhnnyk@gmail.com>
2017-01-10 00:02:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  jurassic5: "What's Golden"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
