7dbe52deb6a8883218bb7a5aac9ecf96507bb7c1
Jessica Widman <jawidman.30@gmail.com>
2017-01-11 12:55:24 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+
+var playlist = new Object({jon:"stamos"});
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
