94999d49ec0c0143f1d6b85fa0b79610ccf7b73b
Elita Ross <chiba213@gmail.com>
2017-01-12 08:16:15 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {name: "title"};
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+function updatePlaylist(playlist, name, title) {
+  playlist[name] = title
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
+function removeFromPlaylist(playlist, name) {
+  delete playlist[name]
   return playlist
 }
