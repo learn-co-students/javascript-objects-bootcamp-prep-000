8a2c0375aa8bc90e0822e38b607b6bd9c9c12ad3
charlie stopek <cstopek@hotmail.com>
2017-01-17 17:30:56 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
-var playlist = {}
+var playlist = {'pearl jam': 'evolution', 'arcade fire': 'haiti'}
 
 function updatePlaylist(object , artistName, songTitle) {
-  return Object.assign({}, object , { [artistName] songTitle })
+  return Object.assign({}, object , { [artistName]: songTitle })
 }
 
 function removeFromPlaylist(object ,artistName) {
