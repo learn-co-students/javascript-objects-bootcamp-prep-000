eb978c0be51fe3b3ddcf6576ed006f23b4267d11
randy manzonzikila <r-manzo@live.fr>
2017-01-21 08:25:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,5 @@
+var playlist = {"booba" : "using"}
+
+function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] = songTitle; return playlist}
+
+function removeFromPlaylist(playlist, artistName) {delete playlist[artistName]; return playlist }
