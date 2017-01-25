01934b03d77052a467db5325910316330390525d
Nathaniel Richardson <richardson.j.nathaniel@gmail.com>
2017-01-25 02:04:01 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,15 @@
-var playlist = new Object({artistName:"songTitle"});
+var playlist = new Object({kanye:"song"})
+
+function updatePlaylist(playlistArg, artistName, songTitle) {
+      playlistArg[artistName] = songTitle
+      return playlistArg
+
+}
+function removeFromPlaylist(playlistArg, artistName) {
+  delete playlistArg[artistName]
+  return playlistArg
+}
+
+//return Object.assign({}, playlist, {[artistName]: songTitle})
+
+//updatePlaylist(playlist, 'Florida', 'songanother')
