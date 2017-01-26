61d887964676eff41c0ac4a183fdc8963da2a9d0
Devin Oulighan <doulighan@gmail.com>
2017-01-26 20:27:03 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  'Tame Impala' : "Eventually",
+  'Modest Mouse' : "Might"
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
