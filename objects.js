ca3f48916b58cdcb88831559e4fe91e0a8f5dc58
Chibueze Osuoji <chibz2006@yahoo.com>
2017-01-27 20:38:58 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist =
+{
+  artistName:"",
+  songTitle:""
+};
+
+function updatePlaylist(playlist, artistName, songTitle)
+{
+  playlist = Object.assign({}, playlist, {[artistName]: songTitle});
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName)
+{
+  delete playlist[artistName];
+  return playlist;
+}
