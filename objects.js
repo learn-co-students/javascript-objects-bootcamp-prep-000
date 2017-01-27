5c71f22dda02c9542c6712ec29503a508dee9a89
Jeremy Esqueda <jeremy@wiredjuice.com>
2017-01-27 02:59:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {artistName: 'songTitle'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return playlist = {'Phil Ochs': 'Here\'s to the State of Mississippi'};
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+
+  return playlist;
+}
+
