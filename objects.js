6e38425fdb35a60f306aa0aaf860436449cd47cf
Lanitta Horner <lanittad@hotmail.com>
2017-01-27 16:55:28 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,15 @@
-var playlist = {Cure: "Pictures of You", "Sheryl Crow": "Tuesday Night"};
+var playlist = {'Cure': "Pictures of You",
+ 'Sheryl Crow': "Tuesday Night"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  // playlist = {}
+  // artistName = "Phil Ochs"
+  // songTitle = "Here's to the State of Mississippi"
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
