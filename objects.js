06dc5b00e0393aee0e358cd806a60b3198e13abe
Sammy Steiner <sammysteiner@gmail.com>
2017-02-02 16:17:20 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {};
+playlist.Beatles = 'letItBe'
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName]= songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
