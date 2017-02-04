7b0e957cba862793a01344e398943b14a339581a
aleksandra shineleva <deadcookies@gmail.com>
2017-02-04 18:46:00 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playList = {artistName: songTitle};
+function updatePlaylist(playList, artist, song){
+  playList[artist] = song;
+  return playList;
+}
+
+
+function removeFromPlayList(playList, artist) {
+  delete playlist[artist];
+
+  return playList;
+}
