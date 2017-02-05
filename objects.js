b5ef2b5e4d2e215167cf7d35f048e31026c4ba90
Tayeb Mc <gomail654@gmail.com>
2017-02-05 18:46:36 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,18 @@
+var playlist ={playlisttype:1};
+
+function updatePlaylist(playlist, artistName, songTitle){
+     playlist = {Phil Ochs:'Here\'s to the State of Mississippi'};
+    return playlist
+}
+
+function removeFromPlaylist(playlist, artistName, songTitle){
+  var playlist= { Kanye: 'Gold Digger'};
+     delete playlist.Kanye;
+    return playlist
+}
+
+
+
+
+
+
