c455e6730da3d84c38257a9e8a3588c98cd70ecb
Francesca Coulibaly <fran.coulibaly@gmail.com>
2017-02-07 16:58:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+
+var playlist = {
+  beyonce : "Single Ladies"
+};
+
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+
+return playlist
+};
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Kanye
+  return playlist
+};
