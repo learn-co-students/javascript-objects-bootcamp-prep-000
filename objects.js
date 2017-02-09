c5743d8400ba7d2d3d1a45549b4c81bb49a74d29
Sarah Denaci <sarahdenaci@gmail.com>
2017-02-09 18:20:58 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object ({'The Smiths': 'How Soon is Now?'})
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist
+}
