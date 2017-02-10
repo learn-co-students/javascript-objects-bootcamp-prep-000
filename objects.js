6cbd71cf60c611770e5565262ee4a76ed737825c
Heather Mason <heatem@gmail.com>
2017-02-10 22:28:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,12 +1,11 @@
-var playlist = {};
+var playlist = { 'Adele': 'Hello'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle;// add song and artists as a key-value pair to playlist
+  playlist[artistName] = songTitle; // add song and artists as a key-value pair to playlist
   return playlist; // return whole playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
-
+  delete playlist[artistName]; // delete the key-value pair from the playlist
+  return playlist; // return the updated playlist.
 }
-// Create a function `removeFromPlaylist` which accepts two arguments (the playlist object and the artist name). The
-// body of the function should delete the key-value pair from the playlist and return the updated playlist.
