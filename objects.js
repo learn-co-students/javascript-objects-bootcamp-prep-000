d22fd6cde9d71ffdd9abbb6ba534afc702a267c1
Vanessa Carrasco <vcarr2010@gmail.com>
2017-02-10 04:01:01 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,13 +4,13 @@ var playlist = {
                 "Big Data" : "The Glow",
                 "AWOLNATION" : "Sail",
                 "MGMT" : "Electric Feel"
-              };
+              };//create new object 'playlist'
 
 function updatePlaylist(playlist, artistName, songTitle) {
     return Object.assign({}, playlist, { [artistName] : songTitle });
-}
+}//new Object using playlist, add artist name and song title
 
 function removeFromPlaylist(playlist, artistName) {
     delete playlist[artistName];
     return playlist;
-}
+}//remove song title by artist name key, using square brackets dt string
