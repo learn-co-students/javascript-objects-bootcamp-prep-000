2d905d49f684698c0e8a3a9b029582ff473abbc4
James Tabush <frozonorb@gmail.com>
2017-02-12 23:29:40 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,12 @@
-var playlist = { artistName: "songTitle" };
+var playlist = { Steven : 'songTitle'};
 
-function updatePlaylist(playlist, artistName, songTitle) {
+
+function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artistName, songTitle) {
+function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist;
 }
