733ac0901e8aa7c81d9e2fcd4a6aea25a4ed29d3
Erika Green <erikagreen7777@gmail.com>
2017-02-14 10:14:44 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,10 +1,13 @@
-var playlist = {};
+var playlist = {
+  muse: "hullabaloo"
+};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle});
 }
 
+
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
- return playlist;
+  return playlist;
  }
