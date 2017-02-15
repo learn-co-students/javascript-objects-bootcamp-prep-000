c3591880a6e920304735faaa8af58c67185bd8d5
Mirna Plakalovic <mirna.plakalovic@gmail.com>
2017-02-15 19:43:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {"Miley Cyrus": "Wrecking Ball"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.'artistName' = songTitle;
+  return {playlist};
+}
+
+function removeFromPlaylist (playlist, artistName) {
+    delete playlist.artistName;
+    return {playlist};
+
+}
