0acc357fd79e4c834e1103d2b3824bb83540cc14
Carlos Almonte <vacarlos_alm@hotmail.com>
2017-02-17 15:21:21 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  artistName:
+  songTitle:
+}
+function updatePlaylist(object, artistName, songTitle) {
+  playlist[artistName] = artistName;
+  playlist[songTitle] = songTitle;
+}
+function removeFromPlaylist(object, artistName) {
+  delete.object.artistName
+}
