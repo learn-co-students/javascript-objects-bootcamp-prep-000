1bd5ffd2ea157e476e513057c730ed615770c0b3
Lia HUANG <lisa.yc.huang@gmail.com>
2017-02-17 22:53:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = {
+  Britney: "Spears"
+};
+
+var updatePlaylist = (playlist, artistName, songTitle) => {playlist[artistName]= songTitle; return playlist;};
+
+var removeFromPlaylist = (playlist, artistName) => {delete playlist[artistName]; return playlist};
