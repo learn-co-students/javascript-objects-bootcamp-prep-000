22b59a00e3eb4f03464f290723e8cac724576a35
subash karki <skarki35205@hotmail.com>
2017-02-18 06:11:25 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { songTitle:" ", artistName: " "};
+
+ function updatePlaylist(playlist, artistName, songTitle){
+      playlist[artistName] = songTitle;
+      return playlist;
+ }
+function removeFromPlaylist(playlist, artistName){
+      delete playlist[artistName];
+      return playlist;
+}
