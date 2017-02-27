0b0857ed2e418570a9818d33e1a82d9a11bc4301
Masaleen Frank <masaleen@gmail.com>
2017-02-27 17:03:59 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+  delete playlist[artistName]
   return playlist
 }
