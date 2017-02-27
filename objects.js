36fe98ef68b6351b8a7ec5b65375d9767a39c975
John Friedrich <friedrichjohn@hotmail.com>
2017-02-27 20:48:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {artistName:"Spoon",songTitle:"Lovin"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
