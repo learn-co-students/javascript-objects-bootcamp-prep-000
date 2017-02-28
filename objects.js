f09cb57b92ccfd14ef5fd105ec7eb27935b1ae7e
Brynne DuBois <brynnedubois@gmail.com>
2017-02-28 22:49:15 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = new Object();
+
+playlist["death cab"] = "Marching Band of Manhattan"
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
