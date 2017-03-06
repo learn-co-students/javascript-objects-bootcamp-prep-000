37f5d18bcf7ff30ee25c775f766fe2793722a5fa
Saul Rueda <saulrp7@gmail.com>
2017-03-06 02:46:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  Artist1: "song1",
+  Artist2: "song2"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
