e01a69a0cab333c6b8ad4845a139421dd1c6bbbf
Sterling Black <sterlingblack13@gmail.com>
2017-03-07 22:25:46 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+
+var playlist = new Object({artist: 'song title'});
+
+function updatePlaylist(playlist, 'artist', 'song title'); {
+  var playlist = {
+    artist: 'song title'
+  }
+  return playlist
+}
+
+function removeFromPlaylist(playlist, 'artist'); {
+  delete playlist.artist
+  return playlist
+}
