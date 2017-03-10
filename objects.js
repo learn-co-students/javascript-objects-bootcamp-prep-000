6a577c3ac9d03639809e4ab2573fc29d48dddffd
Christopher Jones <cjones999983@gmail.com>
2017-03-10 01:04:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { theCalling: 'Wherever You Will Go' };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist
+}
