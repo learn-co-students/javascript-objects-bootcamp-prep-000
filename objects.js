96c05e466abede9d4236a4aab84c96b483e96874
Naz-Al Islam <naz@subtlecoding.com>
2017-03-13 21:41:59 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+    "Michael Jackson": "Beat it"
+  };
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
