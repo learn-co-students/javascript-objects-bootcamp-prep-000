0b1fb952b081e013fb0b0fe810d23d80337a6f7a
Andrew Schittone <a_schittone@yahoo.com>
2017-03-18 18:49:15 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({'kanye' : 'gold digger'});
+
+function updatePlaylist(obj, name, song) {
+  obj[name] = song;
+  return obj;
+}
+
+function removeFromPlaylist(obj, name) {
+  delete obj[name];
+  return obj;
+}
