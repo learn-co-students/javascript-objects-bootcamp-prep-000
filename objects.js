bb1540fb1cc33bdb9a19bd073bf0de07cfb93f11
Caroline Paikkos <carolinefprice@gmail.com>
2017-03-19 19:15:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = { artistName: "songTitle"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return playlist.artistName = "songTitle";
+}
+
+function removeFromPlaylist(playlist, artistName) {
+    return delete playlist.artistName;
+}
+
+/*function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({} playlist {[artistName]: songTitle})
+}*/
