16eb1d05e9f295205f3e19e67f80697982406c60
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2017-03-22 16:47:48 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
-var playlist = {artistNames: 'songTitles' };
+var playlist = {artistName: 'songTitle'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+} ;
