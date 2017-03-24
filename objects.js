08cc9b3ba65631e7590d032cd99169eee28f3f58
Nicholas D'Amico <nickalan82@icloud.com>
2017-03-24 16:34:19 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist = {'Pink Floyd' : 'Wish You Were Here'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
+  // 'destructive' method
+    //  playlist[artistName] = songTitle;
+
+  //  'non-destructive' method returns new string.
+    // preserves original playlist Obj.
   return Object.assign({}, {[artistName]: songTitle});
 }
 
