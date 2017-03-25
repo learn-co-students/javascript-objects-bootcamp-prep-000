89fc79a6d344dbcea47efbd405e9a9206dd75c65
Milly Rodriguez <millz118@gmail.com>
2017-03-25 00:36:29 +0000

    Done.

--- objects.js
+++ objects.js
@@ -10,6 +10,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
+  delete playlist[artistName];
   return playlist
 }
