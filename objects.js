462fbc9414455b0281be8a169319643ed0d7d77a
Jin Li <jinbl19@gmail.com>
2017-03-25 00:46:41 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+
+// const objects = {'this is an object'}
+
+var playlist = {};
+playlist[1] = 'a';
+
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist;
+}
