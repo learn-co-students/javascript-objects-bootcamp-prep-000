24ee5f9a5c69fcf5dc0f875a74dd8f6de7e9135a
David Schlauderaff <dschlaud@gmail.com>
2017-03-26 02:15:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+//var playlist = {}
+var playlist = {artistName: "song title"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] =  songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
