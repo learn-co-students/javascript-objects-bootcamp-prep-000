4a75bf9e33eaccc7ac3693afbf117d0bb726a7ac
mcpccampo <mcp.ccampo@gmail.com>
2017-03-29 02:05:30 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  artistName: '',
+  songtitle: ''
+}
+
+function updatePlaylist(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+function removeFromPlaylist(object, artistName){
+  var obj = Object.assign({},object);
+  delete obj[artistName];
+  return obj;
+}
