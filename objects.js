eb20bc3ba6d96438fb3061f504f468e6846fea4b
Seb Gertz <sebgertz@gmail.com>
2017-04-04 11:21:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {TwoPac: "Neva Be Peace"};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+   return playlist;
+}
