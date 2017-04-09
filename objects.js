a2eb4a642fb502ab9899c0ada0db24767605d4f3
Karen Berba <kberba@gmail.com>
2017-04-09 02:57:08 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
-var playlist = {};
+var playlist = { artistName: 'Alicia Keys', songTitle: 'Girl On Fire'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist;
+
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
