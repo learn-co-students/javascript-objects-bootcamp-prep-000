6c28ba2a1568725e0bc6bc45a77b4120eb0f6af8
Terrance Garr <tgarr88@gmail.com>
2017-04-09 08:31:15 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {artistName: "songTitle"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+
+}
+
+function removeFromPlaylist(playlist, artistName){
+  var playlist = {artistName: "songTitle"}
+  delete playlist.artistName
+  return playlist
+}
