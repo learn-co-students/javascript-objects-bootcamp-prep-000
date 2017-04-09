8156a85956641f0533fc5d6b91cf925886e7d36a
Anthony Verdesca <verdesca2@gmail.com>
2017-04-09 18:57:32 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist= {Stratovarius:'Black Diamond', MagodeOz:'Molinos de Viento'}
+
+  function updatePlaylist(obj, key, value ){
+
+    return Object.assign({}, obj, { [key]: value })
+  }
+
+  function removeFromPlaylist(obj,key){
+
+delete obj.key;
+
+return obj;
+
+  }
