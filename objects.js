52765051434611fc79a18c10f321145fc9bc3595
Mary Obray <maryobray@gmail.com>
2017-04-10 00:00:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {};
+
+function updatePlaylist(object, artistName, songTitle) {
+  object.artistName = songTitle
+  return object
+}
+
+function removeFromPlaylist(object, artistName) {
+  delete object.artistName
+  return object
+}
