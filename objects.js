af129b42a47a030c849389213cc167ab74aa2880
Douglas Horne <dough370@gmail.com>
2017-04-20 17:28:30 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,20 @@
+var artistName;
+var songTitle;
+var playlist = {artistName, songTitle}
+
+//non destructive
+function updatePlaylist(playlist, artistName, songTitle) {
+//function updatePlaylist(artistName, songTitle) {// shorter version
+  return Object.assign({}, playlist, { [artistName]: songTitle })
+}
+
+//destructive
+//function updatePlaylist(playlist, artistName, songTitle) { OR
+/*function updatePlaylist(artistName, songTitle) {
+  return playlist[artistName] = songTitle;
+}*/
+
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
