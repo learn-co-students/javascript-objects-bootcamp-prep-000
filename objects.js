c137ae2d6f1f394299b2a1f3805f70c2b22408bd
Zachary Ciatteo <zciatteo96@gmail.com>
2017-04-23 02:39:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Metallica: "one"
+};
+
+function updatePlaylist(playlist, name, title){
+  playlist[name] = title;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, name){
+  delete playlist[name];
+  return playlist;
+}
