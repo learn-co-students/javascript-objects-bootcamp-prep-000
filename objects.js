2f394345f4abbd7dd1d16f398e95fc1365c739c0
Ginger Wilson <connor.wilson221@gmail.com>
2017-04-24 01:16:49 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,18 @@
+var playlist={
+  // this lists out the keys of artists and values of songs in a object called playlist, each artist can only have one song associated with them inside of an object.
+  benFolds: 'Brick',
+  wonderYears:'Came out Swinging',
+  counterParts:'Choke'
+};
+
+function updatePlaylist(playlist,artistName,songTitle){
+  // this says to add to object(playlist) the key(artistName)  the specified value(songTitle) and then return the new playlist
+  playlist[artistName]=songTitle
+  return playlist
+};
+
+function removeFromPlaylist(playlist, artistName){
+  // this is supposed to say to delete from object(playlist) the key(artistName) to remove the key from the object
+  delete playlist.'artistName';
+  return playlist
+}
