3e97f773059ba2c385d78c71a2c36dd797c24f9b
Matthew Farabaugh <mattfarabaugh50@gmail.com>
2017-04-25 22:45:00 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = {}
+var playlist = {prince:'purple rain'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
