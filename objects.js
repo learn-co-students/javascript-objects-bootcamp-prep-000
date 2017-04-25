8ed9e99d588fdf3d5f84975f8d3b1e1afef693c2
Adames Hodelin <adameshh@gmail.com>
2017-04-25 20:30:16 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'Usher': 'Let it burn'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist
+}
