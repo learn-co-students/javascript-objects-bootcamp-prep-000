06f76ecf6e24a14a124a88bfe391815c65d1d2b1
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-01 13:22:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+playlist = {"en": "hello"}
+
+function  updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
