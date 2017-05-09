0e2388842f7a9545e337cc3b2a6373f758995505
Lawrence Jones <hello@lwjones.com>
2017-05-09 01:27:25 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  anamanaguchi: "Prom Night"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
