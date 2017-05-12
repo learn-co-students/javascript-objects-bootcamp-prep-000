1b5f7a5b1e90c3cd39ebcd9c657a89a5ffefd892
Rory Evenson-Phair <icerobocop@gmail.com>
2017-05-12 02:07:01 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,19 @@
+/*1. Open `objects.js`
+2. Assign an object to the variable `playlist` — the keys will be artist names and the values will be song titles. (What
+limitation does this impose on our `playlist`?)
+3. Create a function `updatePlaylist` which accepts three parameters: the playlist (an object), an artist name (a
+string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist
+object. The function should return the whole playlist.
+4. Create a function `removeFromPlaylist` which accepts two arguments (the playlist object and the artist name). The
+body of the function should delete the key-value pair from the playlist and return the updated playlist. */
+
+//var playlist = new Object();
+var playlist = {artistName: "songTitle"};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist
+}
