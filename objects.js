adcb6f310ad497f4b5dd9e0874e0dea0d2157cf0
Yenifer Prajapati <yen.a@me.com>
2017-05-15 22:01:33 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,3 +2,13 @@ var playlist = {
   Janis: 'Son of a preacher',
   Maroon5: 'Sugar'
 };
+function updatePlaylist (obj, key, value) {
+  return Object.assign( {}, obj, { [key]: value })
+}
+
+updatePlaylist(playlist, 'hello', 'all');
+
+function removeFromPlaylist(playlist, artist) {
+ delete playlist[artist];
+  return playlist;
+}
