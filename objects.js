d0217b2ef1a0db3127b0a7814aad9ed22823e778
Yomaira Escano <yjescano@gmail.com>
2017-05-20 16:56:20 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { Object: '>0' };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return (playlist);
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return ({});
+}
