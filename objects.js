8876cd7baed52130b9b010377cceeac046b91ea1
Danielle Bennett <daniellebennettchan@gmail.com>
2017-05-21 16:04:05 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
