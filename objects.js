a5878ab3114d4d5afdec104afeeb7a6294f25017
Donna Hogan <donnamcla@gmail.com>
2017-05-21 14:59:13 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artist: "song"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  playlist
+}
