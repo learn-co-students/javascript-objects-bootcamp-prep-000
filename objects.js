43136d45b4cee69ff85d939c40f0024eedde1179
David Goodman <davegoodman20@gmail.com>
2017-05-23 18:51:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {};
+playlist.artistName = 'songTitle'
+
+function updatePlaylist(obj, key, value){
+  obj[key] = value
+  return obj
+}
+
+function removeFromPlaylist(obj, key){
+  delete obj[key];
+  return obj;
+}
