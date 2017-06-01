98fd00f96691a93f20476c9b9034dd55df911afa
MaryBeth Dunlap <marybeth_dunlap@yahoo.com>
2017-06-01 20:06:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,3 @@
+var playlist = {artist: 'song'};
+function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName]= songTitle; return playlist}
+function removeFromPlaylist(playlist, Kanye) {delete playlist.Kanye; return playlist}
