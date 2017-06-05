e9652594a92e87b1c12a8464b898f05e81b45086
Lindsey Trainor Trainor <lindseytrainor77@gmail.com>
2017-06-05 23:19:07 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {artist: "title"}
+
+function updatePlaylist(playlist, artist, title){
+
+return Object.assign(playlist,{[artist]:title})
+
+}
+
+function removeFromPlaylist(playlist, artist){
+
+delete playlist[artist]
+
+return playlist
+
+}
