a686679f039629ebda912b40f46b1919f41dbae4
Andrew James <ajames20@gmail.com>
2017-06-08 13:49:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Drake: 'Line em Up'
+};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
