391c55341af47e95602f453a1132de8d7151897d
Stuart Ares <stuares@gmail.com>
2017-06-09 00:22:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  Motorhead: "Ace of Spades"
+  Fleetwood Mac: "some song"
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
