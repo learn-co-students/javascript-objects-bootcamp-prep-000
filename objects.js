cde80c82dfabdb760d054b9d2a325cdcc9b9c4dc
Fred Golini <golinif@hartwick.edu>
2017-06-10 04:03:13 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {future: "dope"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
