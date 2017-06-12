8f6f111148f10134af67a04f8bcb26da938ff3b5
Al Artesona <artesonaa2@gmail.com>
2017-06-12 03:26:40 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,12 +1,10 @@
 var playlist = {Kanye:"famous"};
-
 function updatePlaylist (playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
-
 function removeFromPlaylist (playlist, artistName){
-  var playlist = {}
+  var playlist={}
   delete playlist.artistName
   return playlist
 }
