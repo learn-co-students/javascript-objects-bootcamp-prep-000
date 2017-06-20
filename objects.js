81f433c34a0c527a62bed80d1bee3df133a8dc87
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-06-20 21:46:53 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,17 @@
 var playlist = {
-  Radiohead: "Hello Computer",
-  The Beatles: "Something",
-  Dr Dog: "The Truth"
+  Radiohead: "OKComputer",
+  theBeatles: "Something",
+  DrDog: "theTruth"
 };
+
+function updatePlaylist (playlist,artist,song) {
+  playlist ["artist"] = "song"
+  return playlist
+}
+updatePlaylist(playlist,"Beck","Guero")
+
+function removeFromPlaylist (playlist,artist) {
+  delete playlist.artist
+  return playlist
+}
+removeFromPlaylist("DrDog","theTruth")
