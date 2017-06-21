1b8d846c5530bafcfbc77eef36681944f0d4a301
Andrew Adeniyi <drewadeniyi@gmail.com>
2017-06-21 04:17:37 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ();
+var playlist = new Object ({artistName: 'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
@@ -8,4 +8,4 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
-}
+};
