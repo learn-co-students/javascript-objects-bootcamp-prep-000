52a1e3d912bf7b857068a662cca192d00dde0983
Oliver Curting <oc@hejverden.dk>
2017-06-25 02:21:13 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {"Bob Dylan": "Smoke on the water"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
