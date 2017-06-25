ca54edbf1241a77dbb6a76df4e88105541162f3f
Rick Nilon <r.nilon92@gmail.com>
2017-06-25 19:52:18 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,6 @@
+var playlist = {artistName: "songTitle"}
+
+function updatePlaylist(playlist, artistName, songTitle){[songTitle: artistName]= 'Phil Ochs': "Here's to the State of Mississippi"}
+
+  return playlist
+}
