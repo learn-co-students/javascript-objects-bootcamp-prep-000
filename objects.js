6359dd7631ff504724d52b3e635cef3aeff01fbd
Bastian Bernst <bastian.bernst@googlemail.com>
2017-06-28 09:09:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Madonna: 'Like a Virgin'
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
