793ae01e7487256b5a89408515c1ffb3f44cb053
Frederick Williams <f.williams8602@email.ncu.edu>
2017-06-28 12:55:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist ={'Mike Phillips': 'Wonderful'};
+function updatePlaylist(obj, artist, song) {
+  obj[artist] = song;
+  return obj;
+}
+
+function removeFromPlaylist(obj, artist) {
+  delete obj[artist];
+  return obj;
+}
