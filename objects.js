cd2ad52b7aafbdd229892a6f55abfeea7052c360
Kuntian Yu <yukuntian@gmail.com>
2017-07-03 02:34:21 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {};
+//add function
+function updatePlaylist(playlist, name, title){
+  playlist['name']='title'
+  return playlist
+}
+
+function removeFromPlaylist(playlist, name){
+  delete playlist.name
+  return playlist
+}
