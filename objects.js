314add832f5e97ebc4d290f2c805ae39f4316049
Casey Garland <cccg03@gmail.com>
2017-07-05 15:52:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {artist: 'title'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
