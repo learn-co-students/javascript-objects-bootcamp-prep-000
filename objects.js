b9f60583dc8cfd5ed25399a03ccfb47d2f15657c
Eric Lau <cireual510@gmail.com>
2017-07-12 22:34:11 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,13 +1,13 @@
 var playlist = {Blackbear: 'Do re mi'}
 
-function updatePlaylist(playlist, artistName, songTitle) {
+/*function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle ;
   return playlist
-}
-
-/*function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, (playlist): {[artistName]: songTitle})
 }*/
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, (playlist), {[artistName]: songTitle})
+}
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
