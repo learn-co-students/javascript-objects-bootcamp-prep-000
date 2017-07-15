54894e0ed841c3baeb4b0a6bbeb4df2406b2512a
Hannah Danziger <hdanzig1@jhu.edu>
2017-07-15 18:16:25 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
 var playlist = {artistName: 'songTitle'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
