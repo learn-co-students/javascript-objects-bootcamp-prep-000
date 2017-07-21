5179b39447b3fdd9d37d4524ca11248530b30a94
nathan s <nstrain15@gmail.com>
2017-07-21 13:26:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -3,9 +3,8 @@ var playlist = {Gomez: "It ain't me"}
 function updatePlaylist(playlist, artistName, songTitle) {
   return object.assign({}, playlist, {artistName: [songTitle]})
 }
-/*
+
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
 }
-*/
