30351d9d1f3d2901507b2d74cd509618584e77b5
Kanav Kathuria <kkathuria93@gmail.com>
2017-07-21 21:50:40 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,11 @@
 var playlist = {artistName: "song title"}
 
-function updatePlaylist(playlist, key, value){
-  return playlist
+function updatePlaylist(obj, key, value){
+  obj[key] = value
+  return obj
+}
+
+function removeFromPlaylist(obj, key){
+  delete obj[key]
+  return obj
 }
