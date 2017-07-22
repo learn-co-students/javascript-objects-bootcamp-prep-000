7bdf196c44ae85bf10ab644e3ed21eccf3aaceb7
Stanley Huang Huang <stanhua33@gmail.com>
2017-07-22 07:24:22 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistname;
-  return playlist;
+  delete playlist.artistName;
+    return {}
 }
