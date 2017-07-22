bf15c49d5566dc97bb3e56ef8a0eb7176628d009
Michael Flor <mflor54@gmail.com>
2017-07-22 18:34:35 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  MGMT: "Time to Pretend",
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
