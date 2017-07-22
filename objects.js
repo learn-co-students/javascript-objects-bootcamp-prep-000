9b48dca05426e28dbd806e10ca623540fecc2f3a
Jacob Laurenti <jake.laurenti@gmail.com>
2017-07-22 01:16:02 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var playlist = Object ({ artistName: songTitle })
+var playlist = { artistName: 'songTitle' };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist [artistName] = songTitle
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+function removeFromPlaylist() {
+  delete playlist.artistName;
   return playlist
 }
