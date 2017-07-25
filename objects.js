d369329a699909e41406ee9e369b144f9dab0314
Pujitha Ganni <pujiganni@gmail.com>
2017-07-25 22:13:47 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { TheChainsmokers: 'Closer' }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle })
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
