e4f839d808c599a2f7906bd41e9a806381e2d323
Adam Gold <golddragonxyz@yahoo.com>
2017-07-25 15:26:35 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {"Fleetwood Mac": "Landslide"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
