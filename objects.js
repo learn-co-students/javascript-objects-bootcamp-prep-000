552766b3da4554e2c52a12cb4c7409ef273675ae
Issam Issam <shninder5454@gmail.com>
2017-07-27 00:25:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { Drake: "Over"};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
