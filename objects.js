cc540ea464241cfc7dda9926fb2228062eebe70f
Malachi Price <malachiprice1@gmail.com>
2017-07-27 18:09:02 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
