c95903378da12c23488e4e948ad22378cf54144c
0000000 00000000 <xninfo2015@gmail.com>
2017-08-01 08:40:10 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist ={ name : "a",title:"b"};
+
+function updatePlaylist(playlist,name,title){
+  playlist[name] = title;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist,name){
+  delete playlist[name];
+  return playlist;
+}
