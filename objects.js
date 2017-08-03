295a0812187957a70341d12e12c405e5baef4ec0
Christopher Benedict <chrisjbenedict@gmail.com>
2017-08-03 22:18:35 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Rihanna: "Needed Me"
+}
+
+function updatePlaylist (playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
