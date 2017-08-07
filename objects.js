669555da912db883a765a16f0d144196edb5926a
jbingham223 <jbingham223@gmail.com>
2017-08-07 22:04:52 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {};
+playlist['Ana Tijoux'] = '1977'
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
