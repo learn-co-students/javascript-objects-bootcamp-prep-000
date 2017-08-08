3803f621aabec12573c42fe3bb9efa0f2e788cfb
Brandon Niles <b21niles@gmail.com>
2017-08-08 23:21:29 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  Kanye: 'Gold Digger'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+  return playlist
+
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye
+  return playlist
+}
