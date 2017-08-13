6bbd5f40e8b7fbd13837815282d270bcc663dd0d
Dan O'Dea <dansbands@hotmail.com>
2017-08-13 01:25:00 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { Beatles: 'Revolver'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign( playlist, { [artistName]: songTitle })
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
