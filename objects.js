84d7b7cef3214a49dae3f23872887e07070dfb3b
Steven Snow <stevensno@gmail.com>
2017-08-16 18:20:13 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+
+var playlist = {}
+playlist.artist = []
+playlist.song = []
+
+
+function updatePlaylist(playlist, artist, song ){
+  Object.assign(playlist, {[artist]: song})
+  return playlist
+}
+
+
+
+function removeFromPlaylist(object, artistName){
+ delete playlist.artistName
+  return playlist
+}
