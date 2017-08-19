e3768f7358d7a22a9b9a6a8e0e0af8987597e60a
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-08-19 19:00:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+
+var playlist = new Object({ Beatles: 'Hey Jude'});
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist = Object.assign(playlist, {[artistName]: songTitle});
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
