28a47a6a550163fa65d47348bca257e4ba9bcbd2
Devon Kincaid <devon.kincaid@gmail.com>
2017-08-19 23:32:30 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = { Michael: "Bad"};
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+
+  return playlist;
+}
