b03a3fa410b5c649770f6852ec8653f29dfeb057
Alex Potter <potteralexm@gmail.com>
2017-08-26 20:21:00 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,3 @@
+var playlist = {Beatles: 'Come Together', Britney: 'Crazy'}
+function updatePlaylist(playlist, artist, song){playlist.artist = 'song' return playlist}
+function removeFromPlaylist(playlist, artist){delete playlist.artist return playlist}
