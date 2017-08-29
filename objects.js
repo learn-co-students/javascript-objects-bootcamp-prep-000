99afef6fc24f3def04b3502ff562925af7c584b9
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 16:52:28 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
-var playlist = {artist : "song title"}
+var playlist = {Kanye: "Gold Digger"}
 
 function updatePlaylist(playlist, artist, songTitle) {
-  return Object.assign(playlist, artist, songTitle)
+  return Object.assign({}, playlist, {[artist]:songTitle})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
 }
