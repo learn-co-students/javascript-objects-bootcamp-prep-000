783a7f029f0bcccff1a39168887a55b99200a1a5
Barbara Choy <barbchoy@gmail.com>
2017-08-29 18:22:07 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  'Meghan Trainor': 'No',
+  'Bruno Mars' : 'Count On Me'
+};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName];
+  return playlist;
+}
