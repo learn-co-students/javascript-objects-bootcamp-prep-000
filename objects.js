f3171e2d05678640b4d6ec12ada7967b1d3729a3
Sandy Yeung <sandynism@gmail.com>
2017-08-31 23:58:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = {artistName: 'songTitle'}
+
+function updatePlaylist({}, artistName, songTitle) {
+playlist.artistName = 'Phil Ochs'
+playlist['songTitle'] = "Here's to the State of Mississippi"
+
+}
