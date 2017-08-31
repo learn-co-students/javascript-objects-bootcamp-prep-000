129180689569b63b35635dcfa093368368dd0472
David Kim <davidjkim810@gmail.com>
2017-08-31 15:19:37 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { Muse: "Madness" };
+
+function updatePlaylist (obj, key, value) {
+  return Object.assign({}, obj, { [key]: value})
+}
+
+function removeFromPlaylist (obj, key) {
+delete obj[key];
+  return obj;
+}
