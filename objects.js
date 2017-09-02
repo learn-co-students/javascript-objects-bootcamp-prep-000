328197f5323b6b6a47fe348bca53337800fa7b28
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-09-02 02:15:33 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,18 +6,12 @@ var playList = {
 
 function updatePlaylist(obj, key, value){
 	obj[key] = value
-	console.log(obj)
 }
-console.log(playList)
+
 updatePlaylist(playList, 'theProdigy', 'Omen')
 
 function removeFromPlaylist(obj,key){
 	delete obj[key]
 }
 
-console.log(playList)
-
 removeFromPlaylist(playList,'linkinPark')
-
-
-console.log(playList)
