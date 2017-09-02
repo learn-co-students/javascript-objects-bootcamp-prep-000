be1bb27248900782c0760257f82779f30f3c61ab
Lory Zhang <zhang.rouyun@gmail.com>
2017-09-02 23:19:47 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {artistName: "songTitle"}; 
+
+function updatePlaylist(playlist,artistNames,songTitles){
+  playlist[artistNames]:songTitles;
+  return playlist
+}
+function removeFromPlaylist(playlist,artistNames){
+  delete playlist[artistNames];
+  return playlist
+}
