9a95883e48ed48b729cb70d5c0897d5f40ba66c0
Sarah Wooten <snwooten@gmail.com>
2017-09-07 21:02:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Blondie: "one way or another"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+    playlist[artistName] = songTitle;
+    return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
