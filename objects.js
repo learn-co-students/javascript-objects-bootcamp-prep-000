f25ed72a3442e51ecb38d148a1a4a8480ffbd25a
Sarah Wooten <snwooten@gmail.com>
2017-09-07 21:02:52 +0000

    Done.

--- objects.js
+++ objects.js
@@ -9,5 +9,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
-  return playlist;
+  return playlist; 
 }
