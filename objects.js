52d5e3462bae5454b7690b598d87c9936115287e
Kayla Hansen <shiverr@gmail.com>
2017-09-07 01:08:10 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,10 +1,15 @@
 var playlist = {
-  song 1: "song 1",
-  song 2: "song 2",
-  song 3: "song 3"
+  song: "song 1",
+  sosdd: "song 2",
+  sonasdd: "song 3"
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle })
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye;
   return playlist
 }
