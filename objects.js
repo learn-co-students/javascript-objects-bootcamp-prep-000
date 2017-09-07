18285de2046428ba9ca0c2bff6a7fd0001ed75ab
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-07 20:26:10 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist= {'Kesha': 'Tik Tok', 'Denzel Curry': 'Ultimate'}
+function updatePlaylist{
+  playlist, artist, songtitle
+} 
+{
+  return 
+  playlist.assign(
+    {}, playlist, 
+    {
+      [artist]:songtitle
+    })}
+function removeFromPlaylist{}
