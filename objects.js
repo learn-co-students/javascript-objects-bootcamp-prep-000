9e373a8f7b3dde861b59d45fca89ec308de541b6
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-07 20:42:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,2 @@
 const playlist= new Object({Kesha: 'Tik Tok', Denzel Curry: 'Ultimate'});
-function updatePlaylist{artistName, songTitle} {return Object.assign(playlist,{[artist]:songTitle})}
+function updatePlaylist(playlist, artistName, songTitle) {return }
