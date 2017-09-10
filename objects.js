7608297a926d091a62c82bacddb1ac3ae5076e73
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-10 15:46:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  "Jamie T": "Shela",
+  "Rat Boy": "Scum"
+}
+
+function updatePlaylist(obj, artist, song){
+  obj[artist] = song;
+  return obj;
+}
+
+function removeFromPlaylist(obj, artist){
+  delete obj[artist];
+  return obj;
+}
