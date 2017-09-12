7e42a54b7fb3d8b89f178d42c62e0277bbacd310
Sandy Yeung <sandynism@gmail.com>
2017-09-12 00:04:52 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,11 @@
 var playlist = {artistName: 'songTitle'}
 
-function updatePlaylist({}, artistName, songTitle) {
-playlist.artistName = 'Phil Ochs'
-playlist['songTitle'] = "Here's to the State of Mississippi"
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign(playlist, {[artistName]: songTitle})
+  return playlist;
+}
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
 }
