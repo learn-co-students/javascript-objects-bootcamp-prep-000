63c38398523622ba12f803d0e206b6fcb6f24d1f
jordaninthewind <jordan.kline@gmail.com>
2017-09-13 16:17:41 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {
+  LegionOfMary: 'The Night They Drove Old Dixie Down';
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = songTitle;
+  return playlist;
+}
