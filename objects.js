4452f4b2a09e56df34bb60caba1bde529462eae7
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-13 23:33:43 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {Beyonce:"Drunk in Love"}
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+  return playlist
+}
+function removeFromPlaylist(playlist,artistName){
+  delete playlist['Kanye']
+  return playlist
+}
