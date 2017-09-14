316597811e55c7cc3a4a63a0a45abd35239be43d
Erwan Mailharro <erwan.mailharro@gmail.com>
2017-09-14 09:31:38 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {"Artist Unknown":"Track 1"};
+
+
+function updatePlaylist(playlist,artist,song){
+  playlist[artist]= song;
+  return playlist
+
+}
+
+function removeFromPlaylist(playlist,artist){
+  delete playlist[artist];
+  return playlist
+}
