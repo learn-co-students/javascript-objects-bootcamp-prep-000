e6d3c2591e8928906ac026f8a8cc1c07c3c843c1
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-09-14 11:13:12 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
-var playlist = {}
+var playlist = new Object({"Stavroz": "The Finishing"});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
 }
