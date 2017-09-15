903b4fa961d0fb1929197638dd90300aae4a8585
Lauren  Novotny <laurenmn213@gmail.com>
2017-09-15 17:32:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'Rolling Stones': 'Under My Thumb'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist}
+
+  function removeFromPlaylist(playlist, artistName) {
+    delete playlist[artistName]
+    return playlist
+  }
