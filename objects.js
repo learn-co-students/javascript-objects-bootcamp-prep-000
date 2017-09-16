6368940682158a59a3e7d0c54663915471b37a19
Derek Haller <dhaller.personal@gmail.com>
2017-09-16 00:13:17 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,13 @@
-var playlist = {'Hozier', 'Work Song'}
+var playlist = {
+  Hozier: 'Work Song'
+}
+
+function updatePlaylist (object, artist, songTitle) {
+    object[artist] = songTitle;
+    return object;
+  }
+
+function removeFromPlaylist(object, artist) {
+  delete object[artist];
+  return object;
+}
