9185607591ca85f1f2a7996783e4b6b819930084
nickolas bagnara <nbagnara@gmail.com>
2017-09-17 00:09:15 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {radiohead: 'creep'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
