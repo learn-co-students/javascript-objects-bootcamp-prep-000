9950097e2a3693bf7442efbd2b874ee68db389e3
James Royer <walkingalchemy@gmail.com>
2017-09-24 18:07:15 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Toto : "Africa",
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
