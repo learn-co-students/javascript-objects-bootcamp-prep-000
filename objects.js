fcd0369d4d74209e4cd1233f3207820fbcbd6918
Adam Clayman <adam@clayman.org>
2017-09-28 16:45:25 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  'Chumbawumba': 'Tubthumper'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
