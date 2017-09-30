7c277a9769b9b49063b65ad7cca534f8fab44aa0
apocalypse575 <apocalypse575@gmail.com>
2017-09-30 18:22:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {artistName: 'song titles'}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  return Object.assign ({"Phil Ochs": "Here's to the State of Mississippi"})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  return { }
+}
