df88fd53f683610fb7f4f983d6155261378b85eb
Conor Mooney <cmooney17@gmail.com>
2017-10-02 23:26:50 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = new Object({Artist: "Song Title"});
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
