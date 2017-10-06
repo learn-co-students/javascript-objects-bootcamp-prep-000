35b1e0bf8df1c8c3093de696e00a56e52ce16349
blue shadow <anujose1989@gmail.com>
2017-10-06 06:24:32 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,18 @@
 var playlist = {
-  Adele : "Hello" ,
+  Adele : "Hello",
   Taylor : "I knew you were trouble",
-  Billboard : "Show me the meaning"
+  Billboard : "Show me the meaning",
 };
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+
+  return Object.assign({}, playlist, { [artistName]: songTitle });
+}
+updatePlaylist(playlist,'Phil Ochs', "Here's to the State of Mississippi");
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
+removeFromPlaylist({ Kanye: "Gold Digger" }, 'Kanye');
