c738299d6726f0e41cd5d7d2082b77d67901c72d
Misun Son <misun.son1022@gmail.com>
2017-10-12 23:19:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist={
+  a : 'aa',
+  b : 'bb'
+
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign( {}, playlist, {[artistName] : songTitle})
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+
+}
