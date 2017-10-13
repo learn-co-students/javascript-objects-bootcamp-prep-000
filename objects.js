3cd9991f82c9d3a8324e27c5fd92e10f64f91b72
David Brennan <ddbren@gmail.com>
2017-10-13 20:13:46 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Dagny: 'Wearing Nothing'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
