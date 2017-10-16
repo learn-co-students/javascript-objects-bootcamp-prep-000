73b4e785e6400daba1fdc4af50e7c64ac7419123
Karen G <kgraves0701@gmail.com>
2017-10-16 15:03:52 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
-var playlist = {};
+var playlist = {sade:"smooth operator"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle })
+}
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName]
+  return playlist
+}
