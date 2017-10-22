75fd590485b5d8ad763857f47f0ecb4327468cb3
bruce leong <brucewleong@gmail.com>
2017-10-22 22:28:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {johnnyFlynn: "The Box"};
+
+function updatePlaylist(ob, artistName, songTitle) {
+  playlist.artistName = "songtitle";
+  return playlist;
+}
+
+updatePlaylist(playlist, rickyMartin, "song");
