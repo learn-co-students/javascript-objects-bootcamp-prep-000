538cd984adce4098cd9f019d837961b0ce7de4c4
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:46:56 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,10 +1,13 @@
-var playlist = new Object({artistName : 'songTitle'});
+// assign an object to playlist and initialize the object with an 'artist name : song title' pair
+var playlist = { artistName: 'songTitle'};
 
-function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+// Add the song and artist as a key-value pair to the playlist object
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
-  return playlist
+  return playlist;
 }
