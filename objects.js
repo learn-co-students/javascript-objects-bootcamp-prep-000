c3a2fe6587e3202779043081981a7c52248afd14
Dennis Addo <dennis_addo@aol.com>
2017-10-29 16:22:11 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,11 +6,11 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
