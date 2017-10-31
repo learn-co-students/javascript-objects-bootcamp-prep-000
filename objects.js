97d7062f1dfd1a8b6dd173a327e690b9a6464404
Daniel Lai <dlai813@gmail.com>
2017-10-31 16:29:21 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,3 +1,11 @@
-var playlist = {
-  artist: "titles";
+var playlist = new Object({artistName: 'songTitle'});
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
 }
