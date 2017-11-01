68e0c263ff61541ee94d7ff297eb894052c2a430
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-01 14:05:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+playlist = {
+  TaylorSwift: '22'
+}
+function updatePlaylist(playlist, artist, song){
+  return Object.assign({}, playlist, {[artist]: song})
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist
+}
