bc98632de970738a3bba18237b842541b36b83af
Paul Rojas <rojaspaul@hotmail.com>
2017-11-03 06:18:39 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {}
+
+playlist['Paul Rojas'] = 'El abanico sanyo'
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
