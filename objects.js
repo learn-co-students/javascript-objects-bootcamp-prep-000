d56fc8c282e2e58f88f61762e8212645c50848a2
stephan lalka <slalka@gmail.com>
2017-11-04 23:42:47 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
-var playlist = {R Kelly:"Double Up"}
+var playlist = {"R Kelly":"Double Up"}
+function updatePlaylist(playlist,artistName,songTitle){
+    playlist[artistName] = songTitle;
+    return playlist;
+}
+function removeFromPlaylist(p,a,s){
+  delete p[a];
+  return p;
+}
