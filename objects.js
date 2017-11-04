6d605926f576e2863e12e75a84f2adafc8b452d5
Onur Eker <yonureker@gmail.com>
2017-11-04 00:15:46 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+playlist = {
+  MJ: 'Billy Jean',
+  Maroon: 'Cold',
+  Metallica: 'One'
+};
+
+function updatePlaylist (playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist (playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
