e45e820181243a756c3d0c7eafcfebfee9c8b4dd
Tommaso Padula <tommaso.padula@gmail.com>
2017-11-05 09:22:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,12 @@
 //1
-var playlist = { artistName: "songTitle"};
+var playlist = {artistName: "songTitle"};
 //2
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+//3
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return playlist
+ delete playlist[artistName]
+ return playlist
 }
