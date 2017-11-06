a2ac518e9582dd62bf296cc82e1c846edc11b515
Emanuel Bierman <emanuel.bierman@gmail.com>
2017-11-06 15:47:17 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+
+var playlist = {
+  Chon: "Bubble Dreams",
+  MJ: "Thriller"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist["Phil Ochs"] = "Here's to the State of Mississippi"
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye
+  return playlist
+}
