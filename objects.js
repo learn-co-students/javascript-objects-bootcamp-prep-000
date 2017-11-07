b53302b32c9916a31e1405ce5c10a79a8b39bb63
Erica Pitts <ericavp77@gmail.com>
2017-11-07 21:22:50 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: "songTitle"};
 
 
-function updatePlaylist(playlist, artistName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
