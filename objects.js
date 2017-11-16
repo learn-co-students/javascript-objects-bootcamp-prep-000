f0f043e9da6c3c98fa12c45a660a363458fc78ba
DeAndre Pulliam <deandre.pulliam@yahoo.com>
2017-11-16 21:29:59 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist= {drake:"One Dance"};
 
-function updatePlaylist(playlist, drake, One Dance){
-  playlist'[drake:]=Pop Style';
+function updatePlaylist(playlist, artistName, songTitle){
+playlist[artistName]= songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName, songTitle){
+delete playlist[artistName]
   return playlist
 }
