247ac1170bf9ee8f75d88e7219be6cbe5081d013
Nik <shaione3@gmail.com>
2017-11-21 22:32:08 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'biggie': 'yadayada'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
