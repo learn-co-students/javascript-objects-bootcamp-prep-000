2ec9b9a6fea256a53772344f92dd8096fac1065e
wawan sumardi <wawansumardi@outlook.com>
2017-11-22 02:25:49 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'art': 'song song'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, {artistName: songTitle})
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
