2d8c3d8ea4d79a6f81f3c242c56f512ffb3c6a69
Justin Klepach <justin.klepach93@gmail.com>
2017-12-06 07:44:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  artist: 'song'
+}
+function updatePlaylist (playlist,artist,song) {
+  return Object.assign({}, playlist, { [artist]: song })
+}
+function removeFromPlaylist(playlist,artist) {
+  delete playlist[artist]
+return playlist
+}
