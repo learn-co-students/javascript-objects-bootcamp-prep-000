94808eb39815c71ae28709b3adc0749a12702ce5
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-13 08:31:10 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  artist1: 'title1'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
