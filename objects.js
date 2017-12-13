2ef590c8a0d98ad95ca8e39309811f56a703ee1b
Kim Gonzales <kimgo589@gmail.com>
2017-12-13 22:51:52 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,23 @@
+//curly braces create an Object
+
+var meals ={};
+
+//object constructor
+var meals = new Object();
+
+/*you can initialize an object with key-value pairs when you create it*/
+
+  var playlist = {
+    artistName: 'Song Title'
+    
+  }
+  
+  function updatePlaylist(playlist, artistName, songTitle) {
+    playlist[artistName] = songTitle
+    return playlist
+  }
+  
+  function removeFromPlaylist(playlist, artistName) {
+   delete playlist. artistName;
+    return playlist;
+  }
