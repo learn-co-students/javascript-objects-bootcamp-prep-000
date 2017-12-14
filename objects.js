5a032919bab787e2f72d1e5ca84c5cad13bd9cdd
Ingrid Wong <yw340@georgetown.edu>
2017-12-14 20:05:39 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,33 @@
+
+
+var playlist = {artist: "title"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
+/*  it('defines an object called `playlist`', () => {
+    expect(typeof playlist).to.equal('object')
+    expect(Object.keys(playlist).length).to.be.greaterThan(0)
+  })
+
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      expect(updatePlaylist({}, 'Phil Ochs', "Here's to the State of Mississippi")).
+        to.eql({ 'Phil Ochs': "Here's to the State of Mississippi" })
+    })
+  })
+
+  describe('removeFromPlaylist(playlist, artistName)', () => {
+    it('removes `artistName` from `playlist`', () => {
+      expect(removeFromPlaylist({ Kanye: "Gold Digger" }, "Kanye")).
+        to.eql({})
+    })
+  })
+})
+*/
