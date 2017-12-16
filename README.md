dba98a5623a303ef75b339fcc4287299dc95dfe4
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-16 16:34:43 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript Objects
+```# JavaScript Objects
 
 ## Objectives
 + Explain what an object is in JavaScript is
---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist = {
+  "Kanye": "Gold Digger"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
+
+removeFromPlaylist(playlist, "Kanye");
