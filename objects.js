9c49c49bb57d9132c4cb11b846cc8e92d8c302b3
Sean Oughton <seanoughton@gmail.com>
2017-12-20 18:46:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = new Object({ artist_name: 'song_title' })
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+  
+}
