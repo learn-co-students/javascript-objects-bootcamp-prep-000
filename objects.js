9a26a3c9ebce500ae9968fc77cfece1181e17515
Daniel Cotic <d.cotic26@gmail.com>
2017-12-24 18:25:32 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,10 +4,8 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  return Object.assign({}, playlist, {["Phil Ochs"]: "Here's to the State of Mississippi"})
 }
-
-updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
 
 function removeFromPlaylist(playlist, artistName){
 delete playlist.Kanye
