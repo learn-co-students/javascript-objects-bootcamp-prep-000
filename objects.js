b53866dfb60014d2d5a19bb1d5c5830e22a4c57e
elijahmm17@gmail.com <elijahmm17@gmail.com>
2018-01-11 16:32:28 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  GregIsakov: "That Moon Song"
+};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
