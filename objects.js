117783dab2cb98896ad7e44737d99d786e49324c
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-11 17:42:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { "Katy Perry" : "Roar" }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
