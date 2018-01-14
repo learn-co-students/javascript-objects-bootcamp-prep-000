37e42b0be595988cc7a3e7be80abef80dffb938f
kendocode <git@kendocoding.com>
2018-01-13 23:06:28 -0600

    Done

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
-var playlist = { }
+var playlist = { "Frank Sinatra": "That's Life" }
 
 function updatePlaylist(playlist, artistName, SongTitle) {
   playlist[artistName] = SongTitle
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
+
