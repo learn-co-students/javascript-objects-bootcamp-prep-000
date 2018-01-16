b26763d1b506896ca4e9495bc0c01e53989b47f0
Cassandra Corrales <cmcorrales3@gmail.com>
2018-01-16 03:40:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  artist : 'song'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+    return Object.assign({}, playlist, {
+      [artistName] : songTitle
+    })
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
