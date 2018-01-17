c62d365eda8e6cc98b1410e60ecf3f5b698e5294
Sebastian Schlegel <sebastianschlegel1@gmx.net>
2018-01-17 12:17:54 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { "JohnLennon": "Hello"};
+
+function updatePlaylist(playlist,artistName,songTitle){
+  return Object.assign({}, playlist, { [artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
