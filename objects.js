a43469ccf740a1c0e911fbd994ecb8281540a7d2
Matthew Trenk <matt.trenk@gmail.com>
2018-01-19 03:12:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'beatles' : 'help'}
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
