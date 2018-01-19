08a8f23d8ea091efd0af093453b8e4ae595c2ccb
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-19 21:57:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,18 @@
+var playlist = {eminem:"stan"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  
+playlist[artistName]=songTitle;
+
+return playlist
+  
+}
+
+function removeFromPlaylist(playlist, artistName){
+  
+delete playlist[artistName]
+
+return playlist
+  
+  
+}
