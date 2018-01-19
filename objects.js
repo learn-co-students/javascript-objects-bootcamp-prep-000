fb4f6b8e821d0179d0556f2358bf180e1eb2ae1f
Douglas Craig <dcraig112@gmail.com>
2018-01-19 17:21:29 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,26 +1,10 @@
 var playlist = {beatles: "Hey Jude"}
 
-var artist = 'Wes'
-var songTitle = 'Bumpin on Sunset'
-
-function updatePlaylist(playlist, artist, songTitle) {
-	playlist[artist] = songTitle
-	return playlist
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
 }
-
-function removeFromPlaylist(playlist, artist) {
-	delete playlist[artist]
-	return  playlist
-}
-var artist = 'Wes'
-var songTitle = 'Bumpin on Sunset'
-
-function updatePlaylist(playlist, artist, songTitle) {
-	playlist[artist] = songTitle
-	return playlist
-}
-
-function removeFromPlaylist(playlist, artist) {
-	delete playlist[artist]
-	return  playlist
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
 }
