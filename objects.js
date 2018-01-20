26b03e330bef2b0788a771175bb32288fecdac23
Betsy Sallee <ensallee@gmail.com>
2018-01-20 20:59:10 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Whitney:'No Woman'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['Phil Ochs']="Here's to the State of Mississippi";
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Kanye;
+  return playlist;
+}
