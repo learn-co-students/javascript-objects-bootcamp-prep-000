5f91643a048255084c3d106a0b9c9aba6f769e6d
Adam Bloss <adam.bloss17@gmail.com>
2018-01-21 16:18:10 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = new Object({Rihanna: 'Umbrella'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
