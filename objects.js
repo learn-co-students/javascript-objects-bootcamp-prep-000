19f2609fb3c048d60026009c61e64c43934a992a
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-22 20:00:41 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  daughter: "Smother"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName) {
+ delete playlist[artistName];
+ return playlist;
+}
