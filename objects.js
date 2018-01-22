624ce70eb3cc4baa0386b2958e510f86ad10cada
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-22 20:21:29 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {amyWinehouse: 'Back to Black'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+ playlist[artistName] = songTitle;
+ return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
