72ae155391fcb1897773c1168705fd9a7efa6963
Mark Stephens <mstephens@trackeleven.com>
2018-01-25 14:25:50 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+playlist = {
+'Oasis': 'Wonderwall',
+'Elton John': 'Rocket Man'
+};
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+} 
+function removeFromPlaylist(playlist, artistName) {
+ delete playlist[artistName];
+ return playlist;
+}
