205d882403193278a743972dde0b8578d8be1e81
Alyssa LaPerna <aalaperna@gmail.com>
2018-01-27 18:59:28 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var playlist = { artistName: "songTitle" };
+var playlist = new Object({ artistName: 'songTitle'})
 
-function updatePlaylist (playlist, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
 
-function removeFromPlaylist (playlist, artistName) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
 }
