95b16b942844915fdf7e05fe1683306e3393a2e5
Ronald DeBeras <rdeberas@gmail.com>
2018-01-27 18:12:54 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {'artistName': 'songTitle' }
+
+function updatePlaylist(object, key, value) {
+  return Object.assign({}, object, { [key]: value})
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
