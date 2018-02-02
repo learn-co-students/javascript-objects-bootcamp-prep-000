fef798b62e10b6c9de427f4e200669e12576c7c6
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-02 20:34:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  artistName: "Senses Fail",
+  songTitle: "Still Searching"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+   return Object.assign({}, playlist, { [artistName]: songTitle} ); 
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
