0f654e4255a092f20e214e0040cee0709ec3edbe
Michelle Livengood <ml2876@nyu.edu>
2018-02-07 18:50:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({keys: '0'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
