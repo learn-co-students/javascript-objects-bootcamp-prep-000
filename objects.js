13a15e5d337f7dc7e02895f3834606a33f2c7e1a
Ray Blasko <mray6288@gmail.com>
2018-02-09 12:03:52 -0600

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+
+var playlist = {'artist':'title'}
+
+function updatePlaylist(playlist, artist, title){
+    playlist[artist] = title
+    return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+    delete playlist[artist]
+    return playlist
+}
