661a26f9f8b2d8f53929c12e7f2cef4b8e1e5a6b
Mark Cruz <markchriscruz@yahoo.com>
2018-02-13 05:20:38 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { "Michael Jackson": "Billy Jean" }
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
