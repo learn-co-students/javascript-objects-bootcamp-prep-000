09cceaeb1d96be1a9e1abb60e069d90426e01438
github username <github email address>
2018-02-13 16:08:01 -0800

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { artist: 'song' };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle });
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
