5a3bfebbae7b367c46610cf72de8ace4806db2d9
Raja H <rajahamid12.7@gmail.com>
2018-02-20 00:41:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  artist: 'title'
+}
+
+function updatePlaylist(playlist, name, title) {
+  playlist[name] = title
+  return playlist
+}
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist[name];
+  return playlist;
+}
