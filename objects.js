955534c42af2e09967dc905c2e4c9ec7089d299a
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-20 22:47:27 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { coldplay: 'yellow' };
+function updatePlaylist (playlist, artist, title) {
+  playlist[artist] = title;
+  return playlist;
+}
+function removeFromPlaylist (playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
