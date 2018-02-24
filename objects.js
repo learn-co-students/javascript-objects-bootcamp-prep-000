fd4b9226f1a11968a801d87db36c0067f9739a72
Jerry Brown <brownjer3@gmail.com>
2018-02-24 21:37:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {jayZ: '99Probs'};
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+} 
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
