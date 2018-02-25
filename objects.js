a5cc4849821e8b9acdfe79132d1d48415c232f40
Kaushal Patel <kaushalpatel778@gmail.com>
2018-02-25 20:19:07 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = new Object({artistName: "songTitle"});
+/*var playlist = {
+artistName: "songTitle"};
+*/
+
+function updatePlaylist(playlist, artistName, songTitle) {
+
+playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
