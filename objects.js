485722e391087f2d376a6157a65bde1787c83fb7
Brittany Tinnin <bntinnin7@gmail.com>
2018-02-25 16:47:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { suicidesquad: "purple lamborghini" };
+function updatePlaylist(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function removeFromPlaylist(obj, key) {
+ delete obj[key];
+ return obj;
+}
