d5a4313a294756053427172ba225ec276881cd02
Karim Ezzat <modosaki@gmail.com>
2018-02-27 16:05:27 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { artistName : "songTitle"};
+
+function updatePlaylist (playlist,artistName,songTitle){
+  return Object.assign({},playlist, {[artistName]: songTitle})
+  
+}
+
+function removeFromPlaylist (playlist,artistName){
+  delete playlist[artistName];
+  return playlist;
+
+}
