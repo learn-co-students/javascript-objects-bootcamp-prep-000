6d86fe34725a67ff80197b319dfaabe5b57c8975
Anthony Gawbill <gaw316@gmail.com>
2018-02-27 05:38:49 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {key:'value'
+}
+
+ 
+function updatePlaylist (obj, key, value) {
+  obj[key]=value
+return obj
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName
+  return {}
+}
