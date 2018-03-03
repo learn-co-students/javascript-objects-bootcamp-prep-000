d3226e4c936c7239e286f941471f774b271266f9
fdterr <fdterr@gmail.com>
2018-03-03 16:12:59 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+playlist = {
+  Pink:"What About Us",
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
