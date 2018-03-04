2ffaaf020776a3273a81707fc9ef000552f340be
Pete A <peter.ayresmusic@gmail.com>
2018-03-04 22:48:00 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  Coldplay:'sparks'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+   playlist[artistName] = songTitle;
+   return playlist
+}
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName] 
+  return playlist 
+}
+
+//Object.assign({artistName: ['songTitle'] })
