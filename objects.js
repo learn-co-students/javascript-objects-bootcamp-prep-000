ec3c92e14d206fd0a80ddf7777440dd44159f4e2
karagarvey <kgarvey87@gmail.com>
2018-03-13 20:29:34 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
-var playlist = {}
+var playlist = new Object({artistName:'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({playlist},{artistName:songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  remove playlist[artistName]
+  delete playlist[artistName]
 }
