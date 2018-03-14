f8f987c9cb5c330eecd95f7974b489e4a2869f37
Nancy Jo Chang <enciie@gmail.com>
2018-03-14 23:48:24 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,8 +1,12 @@
-var playlist = { artistName : "songTitle"};
-function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle});
+var playlist = { 
+  'TLC' : 'No Scrubs',
+  'Mandy Moore': 'Only Hope'
+};
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song
+  return playlist
 }
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName];
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
   return playlist;
 }
