ba446a1091844539eb4659f2516b6ed8b2841f24
Rupinder Dhillon <rupa211984@gmail.com>
2018-03-14 22:59:08 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Radiohead: ["Creep", "Paranoid Android"]};
+
+function updatePlaylist (obj, artist, song) {
+  obj[artist] = song;
+  return obj;
+}
+
+function removeFromPlaylist (obj, artist) {
+  delete obj[artist];
+  return obj;
+}
