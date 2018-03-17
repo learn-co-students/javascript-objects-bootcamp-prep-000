31a74720ac540b780113a55734cc71ef6a3dbf25
austin mcclain <mcclainduer@gmail.com>
2018-03-17 00:40:36 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,30 @@
+
+
+var playlist = {
+  taylorSwift: 'Gorgeous',
+  fallOutBoy: 'DanceDance',
+  againstTheCurrent: 'LegendsNeverDie',
+  weezer: 'BuddyHolly'
+};
+
+var updatePlaylist = function(obj,key, value) {
+  obj[key] = value;
+  
+  return obj;
+  
+};
+
+var removeFromPlaylist = function(obj, key) {
+  delete obj[key];
+  
+  return obj;
+};
+
+
+updatePlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi');
+removeFromPlaylist(playlist,'weezer');
+
+var object = {
+  key1: value,
+  key2: value
+};
