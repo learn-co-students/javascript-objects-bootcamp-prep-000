726de0cfb37da695eacc91fb8078dd275ecf55ea
Jocelyn Dombroski <jdombroski@gmail.com>
2018-03-18 00:14:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = new Object({['Artist']: 'Song Title'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
