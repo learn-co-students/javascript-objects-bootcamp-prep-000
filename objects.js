64bf7922d9aaf462685f92bfb2f3244ab8d6dee2
Hollie Lambert <hollielambert@protonmail.com>
2018-03-18 16:48:32 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  "Taylor Swift": "End Game",
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
