c810ec8be9302025f6bfa6917278fabd7de82e5c
Issa Boubacar <bacaris78@gmail.com>
2018-03-21 16:37:59 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { Michael: 'Beat It'}
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+ return playlist 
+}
+function removeFromPlaylist(playlist, artistName){
+ var updatePlaylist = playlist.artistName = {};
+ return updatePlaylist
+}
