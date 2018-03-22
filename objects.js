9f6d23359e918583bee51914e481676d1357fd62
Daila Viteri <dailaviteri@gmail.com>
2018-03-22 17:26:10 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
-  'Foo Fighters': 'Everlong',
-  Prince: 'Purple Rain'
+  'Cardi B': 'Bodack Yellow',
+  Kanye: 'Workout Plan'
 }
 
 function updatePlaylist(playlist, artist, song) {
