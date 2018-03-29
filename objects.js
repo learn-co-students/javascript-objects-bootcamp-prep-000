ce116cef4d7eb1c3250afeee34bcd4f5e8660892
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 08:08:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {artistName:'songTitle',}
+
+function updatePlaylist(obj,key,value){
+  obj[key]=value;
+  return obj;
+}
+function removeFromPlaylist(obj, key){
+  delete obj[key];
+  return obj;
+}
