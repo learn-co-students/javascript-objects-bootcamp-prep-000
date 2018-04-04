5ead1476a6e5ef6baf808f0a446f970fc8f1dd0c
Ekaterina Rodkina <katrinrodkina@gmail.com>
2018-04-04 23:07:57 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,3 +1,14 @@
 var playlist = {
-  artist: song;
+  'The Weekend': 'I feel it coming'
+
+}
+
+function updatePlaylist (playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artistName){
+  delete playlist[artistName] 
+  return playlist
 }
