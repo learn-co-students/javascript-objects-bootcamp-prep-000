8ca153a923539bad0f91bed35cdc8120f566f976
Spencer Croy <spencerccroy@gmail.com>
2018-04-05 02:52:09 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,4 +2,10 @@ var playlist = { SmashMouth: 'Rockstar' };
 
 function updatePlaylist (obj, name, title) {
   obj[name] = title;
+  return obj
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist[artistName];
+  return playlist
 }
