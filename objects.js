d2bd39eb459e5dca0bbcc5a80faa43d5f47c43e7
Kenny Yang <kennyyang19@gmail.com>
2018-04-06 20:28:03 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+
+
+var playlist = {artistName: "songTitle"}
+
+function updatePlaylist(playlist, artistName, songTitle)  {
+
+return playlist[artistName] = songTitle
+
+}
+
+function removeFromPlaylist(playlist, artistName) {
+delete playlist[artistName];
+return playlist 
+}
