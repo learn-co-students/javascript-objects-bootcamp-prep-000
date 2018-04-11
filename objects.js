f23f82382efd2177a7433fc860223ccdf3296f90
Ledio Bode <l.bode515@gmail.com>
2018-04-11 23:09:02 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = new Object ({ artist : "song" });
+
+function updatePlaylist(playlist, artist, song) {
+  
+  playlist[artist] = 'song';
+  return playlist;
+  
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  
+  delete playlist[artistName];
+  return playlist;
+  
+}
