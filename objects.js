2ae0b72736c84d396382340b6d1a5059bcf1c3bc
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-15 20:36:34 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName){
+function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist
 }
