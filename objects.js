8257d8ea1fd041729d8e2091f3c47cd7aceefbad
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-18 02:07:30 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,7 +4,15 @@ playlist["Slowdive"] = "Alison";
 playlist["My Bloody Valentine"] = "Sometimes";
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, { [artistName]: songTitle });
+  playlist[artistName] = songTitle;
+  
+  return playlist;
 }
 
 updatePlaylist(playlist, "Phil Ochs", "Here's to Mississippi");
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  
+  return playlist;
+}
