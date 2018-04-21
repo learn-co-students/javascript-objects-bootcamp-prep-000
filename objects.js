c9d687e582693f7325b7a6d1ab53f7e3b8a3ad3b
Michael Eby <michaeleby1@gmail.com>
2018-04-21 22:16:08 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,8 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: 'songTitle' })
 }
 
-updatePlaylist(playlist, 'My Bloody Valentine', 'Sometimes');
-
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
