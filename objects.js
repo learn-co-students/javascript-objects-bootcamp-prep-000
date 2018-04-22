5e57718c15d48c2a6186cbcddc966689dfb99196
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-22 20:58:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  "The Weeknd": "Trilogy"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
