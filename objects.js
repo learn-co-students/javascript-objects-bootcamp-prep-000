5089966be0104497acc967b44e7ff9e42c8964b7
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-04-24 19:58:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Slowdive:'Alison'};
+
+playlist['My Bloody Valentine'] = 'Sometimes'
+
+function updatePlaylist(playlist, artistName, songTitle) {
+ return playlist['Phil Ochs']= "Here's to the State of Mississippi"
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.Slowdive
+}
