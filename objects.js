e5faa0428df927646175c2c18633addcb3795010
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-02 03:01:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  'artist Names' : 'song names'
+};
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
