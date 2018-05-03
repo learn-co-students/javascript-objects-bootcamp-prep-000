14d5c49aeb7dc77b1cc732ccc06cdf3713c46b6e
Emmanuel Anderson <manu1526@gmail.com>
2018-05-03 10:01:03 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist={artistName:"song"};
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName]=songTitle
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+}
+
+
+
