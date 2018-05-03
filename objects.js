ae6f480cb121686148f6576b757226dafc2a4b06
Rebecca Govert <becci.govert@gmail.com>
2018-05-03 15:37:30 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,20 @@
+var playlist = { artist: 'song' };
+
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+ return Object.assign(playlist, { [artistName]: songTitle });
+ // playlist[artistName] = songTitle;
+// return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
+
+
+  
+
+
+
