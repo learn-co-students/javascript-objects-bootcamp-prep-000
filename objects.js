817326343c6bfe1cee571c7716ce8beba355a06e
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-05 18:05:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  NotoriousBIG : 'Juicy',
+  Kase : 'Javat y Kamel',
+  MuchoMuchacho: 'Tenemos droga'
+};
+
+function updatePlaylist(object, artist, songTitle){
+  object[artist] = songTitle
+  return playlist
+}
+
