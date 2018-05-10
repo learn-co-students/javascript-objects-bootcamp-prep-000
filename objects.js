be4f2b8e052816ff0e7c754737d836370ff35d0f
Paul Bomba` <paul@paulbomba.com>
2018-05-10 17:06:40 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,3 +1,9 @@
-var playlist = {
-  artist: title
+var playlist = { Jamiroquai : "Cosmic Girl" };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign( playlist, { [artistName] : songTitle } );
+}
+
+function removeFromPlaylist(playlist, artistName) {
+    delete playlist[artistName];
 }
