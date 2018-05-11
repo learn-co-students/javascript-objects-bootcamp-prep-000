32607e2cd6de7053072a6f28fc585560e0dc4e48
Luke Worley <luke.worley@colorado.edu>
2018-05-11 07:34:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+   breakfast: "oatmeal"
+};
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
