ab94a0190d08d3cea68c03fb83cc4d4557b9ac55
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-14 23:13:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {'Someone Cool': 'Something Nice'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
