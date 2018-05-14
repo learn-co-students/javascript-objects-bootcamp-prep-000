c96a4dada4b9eabe3a42a5ff8ec05b0973d372ec
thorntonbrenden@gmail.com <thorntonbrenden@gmail.com>
2018-05-14 12:14:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+const playlist = {artist: 'song'};
+
+function updatePlaylist(playlist, artist, song){
+   playlist[artist] = 'song';
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
