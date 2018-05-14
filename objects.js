07f07419400e57b84c69ecaf0d361c8a0c153e8e
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-14 23:14:20 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
 }
 
+
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
 }
