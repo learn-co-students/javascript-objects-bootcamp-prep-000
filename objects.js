b3b77887c0bccc43ff9fbbc5665301dbfa7afc95
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-16 15:37:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {eels : 'fresh feeling'}
+function updatePlaylist(playlist,artist,song) {
+  playlist[artist] = song
+  return playlist
+}
+function removeFromPlaylist(playlist,artist) {
+  delete playlist[artist]
+  return playlist
+}
