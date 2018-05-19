3f1eada97c6284375f53cedf11cd48b5bc687fe8
Kathy <kleboeuf@townisp.com>
2018-05-19 21:50:15 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,15 +1,15 @@
-var playlist = { artistName: 'songTitle' };
+var playlist = { "Barry Manilow": "Can\'t Smile Without You" };
 //console.log(playlist);
 
-function updatePlayList(playlist, artistName, songTitle) {
-   return Object.assign(playlist, {[artistName]: songTitle}
+function updatePlaylist(playlist, artistName, songTitle) {
+   return Object.assign(playlist, {[artistName]: songTitle})
 }
     
 //console.log(updatePlayList(playlist, 'Bonzo', 'Can-Can'))
 
 
-function removeFromPlaylist(artistName) {
-  delete playlist.artistName;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
   return playlist;
 }
 
