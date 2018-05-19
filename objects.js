d4770e8f8aaa89a46fad94aedb6d52cc381a6a96
Kathy <kleboeuf@townisp.com>
2018-05-19 21:18:42 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,12 +1,13 @@
-var playlist = {artistName: songTitle};
+var playlist = { artistName: 'songTitle' };
 //console.log(playlist);
 
 function updatePlayList(playlist, artistName, songTitle) {
-  return Object.assign(playlist, {[artistName]: songTitle})
+  //return Object.assign(playlist, {[artistName]: songTitle})
+   return Object.assign(playlist, {[artistName]: songTitle})
     //playlist.['artistName']: songTitle;
     //return playlist;
 }
-console.log(updatePlayList(playlist, 'Bonzo', 'Can-Can'))
+//console.log(updatePlayList(playlist, 'Bonzo', 'Can-Can'))
 
 
 function removeFromPlaylist(artistName) {
