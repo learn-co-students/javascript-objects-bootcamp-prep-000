225896590d5f55dda3f99ff8875d997b8e608232
stefan.dorianmihai@yahoo.com <stefan.dorianmihai@yahoo.com>
2018-05-22 14:12:21 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist = { Metallica: "MasterofPuppets"};
 function updatePlaylist(playlist, artistName, songTitle){
-return Object.assign({}, playlist, {[artistName]: songTitle});
-
+ playlist[artistName] = songTitle;
+ return playlist;
+}
+function removeFromPlaylist (playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
 }
