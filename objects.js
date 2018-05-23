b2e715ea4b0233de7fef373ed01fe14506140068
Damon Collins <damon.c.p.collins@gmail.com>
2018-05-23 07:05:49 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {Usher: 'Let It Burn'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+ 
+delete playlist[artistName]
+
+}
