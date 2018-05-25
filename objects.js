e316250943adddefd6e6b46163d3add76fb2393f
Princess Shuri <webdevdeja@gmail.com>
2018-05-25 19:16:50 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = {janelleMonae: 'I got the juice', kaliUchis: 'Ridin Round', todrickHall: 'T.H.U.G.'};
+
+function updatePlaylist(obj, key, value){
+  Object.assign({janelleMonae: 'I got the juice', kaliUchis: 'Ridin Round', todrickHall: 'T.H.U.G.', jaro:'Annabelle'});
+  return playlist;
+}
+
