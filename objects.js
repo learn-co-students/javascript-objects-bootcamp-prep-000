0a8e4aa575c2e2217525baccd9df2c16f141f8af
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2018-05-28 16:23:37 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {Object}
+function updatePlaylist(playlist, artistName, songTitle) {
+playlist[artistName] = songTitle
+return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName] 
+}
