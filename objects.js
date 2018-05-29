5b4600c5ced20370faa6417bf2590d66be2c221c
Kyle Spencer <spencerk2@vcu.edu>
2018-05-29 22:54:29 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,12 @@
 var playlist = {CarrieUnderwood: ' Two Black Cadillacs'}
 
-function updatePlatlist (playlist, a, b) {playlist[a] = 'b'; return playlist}
+function updatePlatlist(playlist, a, b) {
+  playlist[a] = 'b';
+  return playlist
+}
 
-function removeFromPlaylist (playlist, a) {delete playlist[a]; return playlist}
+function removeFromPlaylist(playlist, a) {
+  delete playlist[a];
+  return playlist
+}
 
