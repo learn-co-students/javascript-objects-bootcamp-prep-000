6420fe96269fc099478973086c06603fd40478ee
Hannah Hughes <hugheshannah96@gmail.com>
2018-05-29 22:53:29 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  hannah: "Hannah's song"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
