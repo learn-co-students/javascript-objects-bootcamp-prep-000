2bc985011830c66254fa11fa849ca1370a92ddc2
jose barboza <jrbarboza08@gmail.com>
2018-05-29 22:24:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {ACDC: "School of rock"};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+ 
+  return playlist[artistName] = songTitle};
+  
+ function removeFromPlaylist (playlist, artistName) {
+    delete playlist [artistName]
+ }
