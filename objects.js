67366e0624318bae5bb55c180da0241e7cb4b669
Richard Melchior <raustin.melchior@gmail.com>
2018-05-30 23:49:31 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  billyJoel: "Piano Man"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign( playlist, {[artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+}
