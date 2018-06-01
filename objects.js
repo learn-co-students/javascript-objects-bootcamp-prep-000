83711e8073072ef76e5d4282ba73829ceaebb096
Shravani PL <pl.shravani@gmail.com>
2018-06-01 18:45:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {artist:name};
+
+function updatePlaylist(playlist,artistName,songTitle) {
+  
+playlist[artistName] = songTitle
+return playlist
+}
+
+function removeFromPlaylist(playlist,artistName) {
+ 
+delete playlist[artistName]
+ 
+}
