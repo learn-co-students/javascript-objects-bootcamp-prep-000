8af44715e5073a69cb3ba496f33125c761ee822f
Sarah Dilworth <sarah.petty18@gmail.com>
2018-06-01 13:06:52 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { 'Slowdive' : 'Alison', 'My Bloody Valentine' : 'Sometimes'};
+
+function updatePlaylist(playlist) {
+playlist['Phil Ochs'] = "Here\'s to the State of Mississippi";
+return playlist;
+}
+
+function removeFromPlaylist(playlist) {
+delete playlist.Slowdive;
+return playlist;
+}
