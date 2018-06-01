53e7e97799ef11452c52a54cf3d1689a9a2fd588
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-01 20:36:52 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artist: 'song'};
+
+function updatePlaylist(playlist,name,song) {
+  playlist[name] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist,name) {
+  delete playlist[name];
+  return playlist;
+}
