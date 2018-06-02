3ca28363ce6aeeeac4486c23c153d785f868b750
Nicholas  Luong <nckluong@gmail.com>
2018-06-02 03:19:21 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artist: 'song'};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
