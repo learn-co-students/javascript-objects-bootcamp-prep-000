a88b54a77903bb1f06adb6251ab780233175c036
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 00:16:28 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,19 @@
+/** Declared playlist as a var object **/
 var playlist = {
   billyJoel: "Piano Man"
 }
 
+/**
+ * updatePlaylist function takes in a playlist object, artistName, and songTitle and assigns the playlist a new key value pair using
+ * the artist name and song title
+ **/
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign( playlist, {[artistName]: songTitle});
 }
 
+/**
+ * removeFromPlaylist function takes in a playlist and artist's name and deletes the key denoted by the artist's name
+ **/
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
 }
