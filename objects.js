18c4315e90daeaa8f764d633460f9d2c2218411e
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-02 19:32:50 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Beatles: 'Yellow Submarine'}
+
+function updatePlaylist(playlist, artist, song) {
+ playlist[artist] = song
+ return playlist 
+}
+
+function removeFromPlaylist(playlist, artist) {
+ delete playlist[artist]
+ return playlist 
+}
