3d0773e306ea60adbf07e9bbd537eb13373f0825
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 03:23:16 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
+
+function updatePlaylist(playlist, name, title) {
+  return playlist['Phil Ochs'] = "Here's to the State of Mississippi";
+}
+
+function removeFromPlaylist(playlist, name) {
+  return delete playlist.Slowdive
+}
+
