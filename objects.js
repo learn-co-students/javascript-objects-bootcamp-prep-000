61cc074e8a0f15b8ddb90cdbe429bfe8ab9a569c
Amalie S <amaliesmidth@gmail.com>
2018-06-03 19:32:41 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  Slowdive: 'Alison',
+  'My Bloody Valentine': 'Sometimes'
+}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
