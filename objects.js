1bb75525814c82c5211c28ec6bf3ed0164a4b141
heii70 <williamgao70@yahoo.com>
2018-06-06 21:00:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { testKey: "testValue" };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
