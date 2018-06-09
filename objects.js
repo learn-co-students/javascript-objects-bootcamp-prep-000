4da6772660ae0720e97bb07cdac6094f11eb60ae
akaminet@umich.edu <akaminet@umich.edu>
2018-06-09 03:04:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {beyonce: "Halo"}
+
+function updatePlaylist(obj, artist, song){
+  obj[artist] = song
+  return obj
+}
+
+function removeFromPlaylist(obj, artist){
+  delete obj[artist]
+}
