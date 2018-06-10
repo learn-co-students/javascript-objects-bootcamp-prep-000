cab5a57e479aaa1f4c41535005902c355ee8752f
jose barboza <jrbarboza08@gmail.com>
2018-06-10 21:31:19 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,9 +1,27 @@
+<<<<<<< HEAD
 var playlist = {ACDC: "School of rock"};
 
 function updatePlaylist (playlist, artistName, songTitle) {
  
-  return playlist[artistName] = songTitle};
+  return playlist[artistName] = songTitle
+  
+};
   
  function removeFromPlaylist (playlist, artistName) {
     delete playlist [artistName]
  }
+=======
+var playlist = {
+artistName:"songTitle"
+  
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artistName){
+ delete playlist[artistName];
+return playlist;
+>>>>>>> b18198f539d9ccda38175760bea9d4275c2a5f97
