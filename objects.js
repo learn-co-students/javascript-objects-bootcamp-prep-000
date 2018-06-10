6d6b2ff1c1c5a12a31740d2cfcc26142bc05834b
T L <tom.kennedy.lee@gmail.com>
2018-06-10 11:33:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {Slowdive: 'Alison'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+  return playlist 
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist['Slowdive']
+  return playlist
+}
+
