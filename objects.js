29bbd07d23a59b6573e25aa49a79312979a61bc6
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-11 05:22:56 +0000

    Done.

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist
 }
