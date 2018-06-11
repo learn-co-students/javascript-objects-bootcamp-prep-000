d906b5ec1e6b7d24da31c7d9aef678964770f2d5
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-11 04:29:28 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Jon_Foreman: "Your Love is Strong"
+}
+var artistName
+var songTitle
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
+}
