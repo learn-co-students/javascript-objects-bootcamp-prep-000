8fc963bc97051cd4ae221c4fd5d325aa9516ddb3
Daniel Johnson <danielfrankjohnson@gmail.com>
2018-06-12 20:34:53 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,14 @@
 var meals = {};
 var meals = new Object();
 
-var playlist = { savagegarden: "truly madly deeply" };
+var playlist = { savagegarden: "Truly Madly Deeply" };
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];   
+  return playlist;
+}
