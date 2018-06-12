e238fea22114470d22642ab5bcccec6e5f222e79
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-06-12 17:30:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {beyonce: "end of time"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive
+  return playlist
+}
