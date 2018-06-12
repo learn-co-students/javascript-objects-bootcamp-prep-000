c3dde9011fb72b8ad1d554b131e6e5568b05e046
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-06-12 11:04:17 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,13 +1,13 @@
 
 
-var playlist = {coldplay: yellow, eminem: "rap god"}
+var playlist = {coldplay: "yellow", eminem: "rap god"}
 
 function updatePlaylist(object, artist, song) {
-  object.artist = ["song"]
+  object[artist] = song
   return object;
 }
 
 function removeFromPlaylist(object, artist) {
-  delete object.artist;
+  delete object[artist];
   return object;
 }
