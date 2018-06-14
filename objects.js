e206564f621b5c8b28ffb7c07165f76863e615a2
Voumo Tiogo Joel Brice <tiogoj@hotmail.com>
2018-06-14 12:47:46 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,14 +1,15 @@
+// initialize the playlist object
 var playlist = {
-  artistName: 'Addele',
+  artistName: 'Adele', // key value = artistName and the value is Adele
   artistTitle: 'hello'
 }
 playlist
-
+// update function
 updatePlaylist(playlist,artistName, songTitle){
    return Object.assign({playlist},artistName,songTitle)
 }
 playlist
-
+// remove function
 function removeFromPlaylist(playlist, artistName){
   delete playlist[0]
   return playlist
