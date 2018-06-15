f155c8fcd6985a5ecd379a481f07c40b0bb2cf1a
Austin Powell <austinpowell58@gmail.com>
2018-06-15 01:31:50 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
-var playlist = {kesha: "Isaac Song"}
+var playlist = {kesha: "Isaac Song"};
+
 function updatePlaylist(obj, key, value) {
-  return Object.assign( obj, { [key]: value })
+  return Object.assign( obj, { [key]: value });
   }
 function removeFromPlaylist(obj, key) {
-  delete obj.key;
+  delete obj[key];
   return obj;
 }
