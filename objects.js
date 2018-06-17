92520937255f871eb0ad42acddb38fafef8994a4
nmkleiner@gmail.com <nmkleiner@gmail.com>
2018-06-17 07:09:35 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {'Bob Marley' : 'Kaya'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
