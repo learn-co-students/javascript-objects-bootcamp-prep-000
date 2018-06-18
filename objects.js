109208d38155d9f6b3cb608565b3d4cb389e681d
Umyippa <sywhitehouse@gmail.com>
2018-06-18 16:02:44 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
-var playlist
+var playlist = { artistName: "songTitle"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi" ;
+  return playlist;
+  
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive ;
+  return playlist;
+}
