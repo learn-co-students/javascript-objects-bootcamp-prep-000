9699b3996cc190d4f1309ade53d9f77aa8f7d7c1
ksiman14@gmail.com <ksiman14@gmail.com>
2018-06-19 18:54:32 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  artist: "song title"
+}
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
