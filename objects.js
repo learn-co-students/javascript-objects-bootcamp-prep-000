100424d937ced2ba73c26c169c910edf93ff4d91
Derrick Skipwith <dskipwith15@gmail.com>
2018-06-19 22:45:07 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist = {[
-  artistName]: 'songTitle'
+var playlist = {[ 'Big Sean' : 'Feelings'
+  ]: 'songTitle'
 };
 function updatePlaylist (playlist, artistName, songTitle) {
  Object.assign({}, playlist, {[artistName]:songTitle})
