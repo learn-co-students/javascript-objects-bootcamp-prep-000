3d97d044515b4dff0087ffc79db3b594748cc053
Derrick Skipwith <dskipwith15@gmail.com>
2018-06-19 22:30:32 +0000

    Done.

--- objects.js
+++ objects.js
@@ -3,12 +3,17 @@ var playlist = {
 }
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
-<<<<<<< HEAD
+
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
 }
-=======
+
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
+
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
