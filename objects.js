ca3423f65cc6b9caad0c3663aeb35088e5f80e79
Derrick Skipwith <dskipwith15@gmail.com>
2018-06-19 19:37:14 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,24 +1,9 @@
-var recipes = {};
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value});
+var playlist = {
+  artist: "song title"
 }
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
 }
-
-function deleteFromObjectByKey(object, key) {
-  var clone = Object.assign({}, object);
-  delete clone[key];
-  return clone;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
 }
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> de7e46b2009e6bdb989e6e135db785aa645253e8
