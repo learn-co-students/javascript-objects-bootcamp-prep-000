5feff7b582ab49a785cda75b1674b94adcfc2de6
Farid Mansour Thabet Alabsi <alabsifarid@gmail.com>
2018-06-25 09:09:11 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  'Marine Boudeau': 'Something',
+  'Someone else': 'Something else'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
