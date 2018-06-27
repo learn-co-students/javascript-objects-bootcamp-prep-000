09b0486a697d12d065e73ce766e502a38de0b875
Jason Stone <jason.stone0808@gmail.com>
2018-06-27 01:31:46 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  'Phil Ochs': 'Tell Me',
+  Prince: 'Purple Rain'
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
+  return playlist
+}
