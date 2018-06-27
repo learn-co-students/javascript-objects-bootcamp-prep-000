ee7a1fdfbb37909a6c00b1538e535ab814e47a1e
kalex19 <klewis@rockymountainprep.org>
2018-06-27 03:12:17 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,5 @@
+var playlist = { artist name: "song title"}; 
+function updateplaylist (playlist, atrist name,song title )
+{ return playlist } 
+function removeFromPlaylist ( playlist, artist name)
+{ return object.assign ({}, playlist) }
