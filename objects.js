ee7f84ebe39638ed1e6679c6ff68bfff65d2e1d8
Carie Pigeon <cariepigeon81@gmail.com>
2018-06-29 13:19:02 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var playlist = {'Glen Hansard': "Tell it to me now"};
 function updatePlaylist(playlist, name, song) {
   return Object.assign(playlist, {[name]: song});
 }
-//updatePlaylist(playlist, 'name', 'song');
+updatePlaylist(playlist, 'name', 'song');
