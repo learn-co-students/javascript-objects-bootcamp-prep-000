ae6d65138ca33cb5ca10d56dc4e2ddb8808ce2c7
code23nr@gmail.com <code23nr@gmail.com>
2018-07-03 01:43:39 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
-var playlist = {artistName:"songTitle"};
+var playlist = {artistName:'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-
-  return Object.assign(playlist,{[artistName]:songTitle})
+  return Object.assign(playlist, {[artistName]:'songTitle'})
 }
 
 function removeFromPlaylist(playlist, artistName) {
