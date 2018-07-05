150c5b04403b1092ced19145d1f130b30c74aba6
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-05 22:56:13 +0000

    Done.

--- test/objects-test.js
+++ test/objects-test.js
@@ -47,3 +47,12 @@ describe('objects', () => {
     })
   })
 })
+
+var playlist = {Sinatra: "Summer Wind"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+function removeFromPlaylist(playlist, artistName, songTitle) {
+  delete playlist[artistName];
+}
