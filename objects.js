dac4c8def33727a465bda67f4557d7ab83d188f1
Benjamin Lee <nycxbl@hotmail.com>
2018-07-06 17:31:20 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {};
+
+function updatePlayList (list, artist, title) {
+  Object.assign({}, list, { artist: [title]})
+  return object;
+}
+
+function removeFromPlaylist(object, name) {
+  Object.assign({}, list, { artist: [title]})
+  return object;
+}
