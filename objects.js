3cccadc7a87dd9fa39f94b59dd537d18e1f08518
Aleksandar Gakovic <algakovic@gmail.com>
2018-07-10 05:00:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {A7X: " A little piece of Heaven"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
