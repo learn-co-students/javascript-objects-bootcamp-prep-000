801faa35ce0be34d228e3fe09dd57abfcd890f81
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-10 12:17:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {"Gym Class Heroes": "Cupid's Chokehold"}
+
+
+function updatePlaylist(playlist,artist,song){
+  playlist[artist] = song
+  return playlist
+  
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete [playlist.artistName]
+	return playlist;
