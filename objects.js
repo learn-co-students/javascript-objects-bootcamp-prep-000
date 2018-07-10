da08487c5457d1f9a5237eaa470f77a6c0bdf050
John Higgins <johnhiggins@outlook.ie>
2018-07-10 20:06:12 +0000

    Done.

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 }
 
 function updatePlaylist (playlist, artistName, songTitle){
-  playlist ['Phil Ochs'] = ["Here's to the State of Mississippi"]
+  playlist [artistName] = songTitle
 }
 
 function removeFromPlaylist (playlist, artistName){
