42e1cc9dc2b82510fbaff9fd5654a1744224656d
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-07-13 18:13:30 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist={artistName: 'songTitle'};
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName]=songTitle
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
