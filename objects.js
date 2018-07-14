a108ed46e011d5d7b47fbb216b8b1ed302a6cd49
timothy.knoll@gmail.com <timothy.knoll@gmail.com>
2018-07-14 14:01:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  artistName: 'Michael Jackson', songTitle: 'Thriller',
+ 
+}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  
+}
+
+function removeFromPlaylist (playlist, artistName){
+  
+  
+}
