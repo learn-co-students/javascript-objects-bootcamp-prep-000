2adcc315901923458c23d2e93adae44f12b8f245
Amy Zhou <aymeighz@gmail.com>
2018-07-16 21:04:48 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = {artistName: 'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = 'songTitle';
-  return updatePlaylist;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
-  return removeFromPlaylist;
+  return playlist;
 }
