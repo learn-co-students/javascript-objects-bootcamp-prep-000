a96c727050bcb9d134eefd1acb749fb384c9fb05
Edward Castillo <edwardcastillo@me.com>
2018-07-16 23:47:01 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,16 +1,13 @@
-var playlist = {artsistName: "songTitles"};
+var playlist = {
+  artistName:"songTitle"
+}
 
-function updatePlaylist(playlist, artistName, songTitles) {
-
-playlist[artistName] = songTitles;
-return playlist;
-
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
-
-delete playlist[artistName];
-playlist;
-return playlist;
-
+  delete playlist[artistName];
+  return playlist;
 }
