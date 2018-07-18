61d1d659b2f6f5b3bc78da63c7c9ae2403bdcd45
elaitman1 <elaitman1@gmail.com>
2018-07-18 23:59:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+let playlist = {
+"Michael Jackson" : "Thriller"
+}
+
+function updatePlaylist(playlist1, artistname, songtitle)
+
+{
+playlist1[artistname];
+playlist1[artistname] = songtitle;
+return playlist1;
+}
+function removeFromPlaylist(playlists, artistname){
+  delete playlists[artistname];
+  return playlist;
+}
