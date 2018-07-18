5d78496166f78b149fc95cd6977ae9b066c870a7
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 13:22:16 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,14 @@
-var playlist = new Object({artistName: songTitle});
+var playlist = new Object({
+    Slowdive: 'Alison',
+    'My Bloody Valentine': 'Sometimes'
+});
+
+/* if i want to add to a playlist non-destructively i can use the below code - tested & works in chrome web brower console
+
+function updatePlaylist (playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: songTitle} );
+}
+
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
+*/
+
