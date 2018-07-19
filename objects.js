2b33040f0762d92828817a224373a4bd272a83d3
jvpink200 <jvillarr20@gmail.com>
2018-07-19 00:34:00 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var meals = {breakfast:"oatmeal"};
+var meals = new Object({ breakfast: 'oatmeal'});
+
+
+meals[firstMeal];
+
+meals.snack = 'yogurt';
+
+var playlist = {};
+
+function updatePlaylist(playlist,artistName, songTitle){
+
+}
