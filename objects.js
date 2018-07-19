3cf56d41c5e5f8cea1f9c147d33593d2cd945189
Hannah Do <hannah.hj.do@gmail.com>
2018-07-19 00:39:19 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,8 +1,5 @@
 var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
 
-      playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
