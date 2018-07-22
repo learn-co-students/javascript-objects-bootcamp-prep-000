61837140c6a61377e28f03c65e2b75f3b9c1eb13
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-22 09:20:27 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+ var playlist = new Object({
+  'Slowdive': "Alison",
+  'My Bloody Valentine': "Sometimes"
+});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+ playlist['Phil Ochs'] = "Here's to the State of Mississippi";
+ return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist["Slowdive"];
+  playlist;
+}
