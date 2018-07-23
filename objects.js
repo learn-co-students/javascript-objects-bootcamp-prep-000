9cf7c4716c6884cc8da57aa19e9b981b8de3e2a2
min kim <minwookkim2013@u.northwestern.edu>
2018-07-23 15:15:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {deadmau5: 'strobe'}
+
+function updatePlaylist(playlist, artist, title){
+return Object.assign({},playlist,{[artist]:title})
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
