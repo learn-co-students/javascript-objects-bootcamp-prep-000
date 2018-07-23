0eec9c19bedb1c093d883de0ef23c191ccba8c32
Olivier Willems <ojwillems@gmail.com>
2018-07-23 00:10:21 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {theBlackKeys: 'Tighten Up'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  
+  return playlist
+}
