7b1acf4571bce1c6daa3a0b531749d7e1f3dc9bc
Brett Portnoy <brett.portnoy@gmail.com>
2018-07-25 15:48:52 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  artist: "song title"
+};
+
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist;
+}
