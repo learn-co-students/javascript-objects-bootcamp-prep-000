a27bb617d7bb35d4791a7eef4ad8bafaeb9ef13e
Juna Luzi <junaluzi@gmail.com>
2018-07-27 02:55:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'Coldplay' : 'The Scientist'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
