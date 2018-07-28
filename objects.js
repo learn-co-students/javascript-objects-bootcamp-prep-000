e97346074caf144944f9fc60edb0e3981c5f924c
Amir Safoev <amirsafoev0@gmail.com>
2018-07-28 13:58:05 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var  playlist = {artistNames: 'song titles'}
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
+function removeFromPlaylis(playlis, aristName){
+delete playlist.artistName;
+return playlisi
+  
+}
