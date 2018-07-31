778be6b0d10501b390444407b365568eba0b65f1
TheCodingAlchemist <40527019+TheCodingAlchemist@users.noreply.github.com>
2018-07-31 02:39:27 -0400

    Delete objects.js

--- objects.js
+++
@@ -1,8 +0,0 @@
-var playlist = {Chevelle: "Roswells Spell"};
-function updatePlaylist(object, artistName, songTitle) {
- return Object.assign({}, playlist, {[PhilOchs]: "Here\'s to the State of Mississippi"});
-}
-function removeFromPlaylist(object, artistName) {
-  delete playlist.Slowdive;
-  return playlist;
-}
