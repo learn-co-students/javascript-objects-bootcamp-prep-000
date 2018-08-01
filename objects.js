6690d256ea679e33704ed20cf40a4629ae45e4ff
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-01 11:03:27 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+playlist = { Keane: 'Everybody\'s Changing'};
+
+function updatePlaylist(list, artist, song) {
+  list.artist = song;
+  return list;
+}
+
+function removeFromPlaylist(list, artist){
+  delete list.artist;
+  return list;
+}
