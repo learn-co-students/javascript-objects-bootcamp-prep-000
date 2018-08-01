d2b8e4d7948f663a6b0dbabe9e06a6397744ec36
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-01 11:56:14 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-playlist = { Keane: 'Everybody\'s Changing'};
+var playlist = { Keane: 'Everybody\'s Changing'};
 
 function updatePlaylist(list, artist, song) {
-  list.artist = song;
+  list[artist] = song;
   return list;
 }
 
 function removeFromPlaylist(list, artist) {
-  delete list.artist;
+  delete list[artist];
   return list;
 }
