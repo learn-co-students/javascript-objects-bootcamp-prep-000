f0f6f3c0cb0181d1df96e97573e18ce37f5b7e08
Albert Chun <albert.s.chun@gmail.com>
2018-08-02 03:36:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = { 
+  "ACDC": "Thunderstruck", 
+  "Jay Rock": "Win",
+  "JT": "Sexy Back"
+};
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
+}
