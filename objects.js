9db5264aef7f0d69fba20060a452315887a8fb64
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-03 16:12:33 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {"Wolf Alice": "Don't Delete the Kisses"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
