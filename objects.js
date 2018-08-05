77a7a59607be74ba68b93f471209e8277a3ef970
Michael Barbone <michael.barbone@gmail.com>
2018-08-05 01:24:20 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Slowdive: "Alison", My Bloody Valentine: "Somtimes"};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist.PhilOchs = "Here's to the State of Mississippi";
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.Slowdive;
+  return playlist;
+}
