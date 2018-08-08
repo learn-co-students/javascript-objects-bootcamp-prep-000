e8e4d9723b0abcc9ec3a41c7f1f08cbd835a5588
Cris Hanks <crishanks12@gmail.com>
2018-08-08 23:34:05 +0000

    Done.

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 };
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
+  delete playlist[artistName];
 }
 
 
