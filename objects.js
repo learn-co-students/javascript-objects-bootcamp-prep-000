cd5803ad026be21e9e8c577870bc9a75314d4493
Cris Hanks <crishanks12@gmail.com>
2018-08-08 23:10:44 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,59 +1,17 @@
-// function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
-//   obj[key] = value
-//   return obj
-// }
-
-// const recipe = {
-//   eggs: 3
-// }
-
-// destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
-
-// Object.assign({}, {foo: 'bar'})
-
-// Object.assign({eggs: 3 }, { flour: '1 cup'})
-
-// Object.assign({eggs: 3}, {chocolate: '1 cup'}, {flour: '1/2 cup'})
-
-// function updateObjectWithKeyAndValue(obj, key, value){
-//   return Object.assign({}, obj, {[key]: value})
-// }
-
-// function updatedObjectWithObject(targetObject, updatesObject){
-//   return Object.assign({}, targetObject, updatesObject)
-// }
-
-// var meals = {
-//   breakfast: 'oatmeal',
-//   lunch: 'turkey sandwich',
-//   dinner: 'steak and potatoes'
-// };
-// delete meals.dinner;
-
-// meals.breakfast = ['oatmeal', 'banana']
-
-// Object.assign({}, meals, ['oatmeal', 'banana'])
-
 var playlist = {
-  artistName: 'songTitle'
-};
-
+ artist: 'song' };
+ 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign(playlist, {[artistName]: songTitle})
+  playlist[artistName] = songTitle
+  return playlist
 };
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
-  return playlist{}
-};
+  delete playlist.artistName;
+}
 
 
 
-
-
-
-
-
-
-
-
+// function updateObjectWithKeyAndValue(obj, key, value){
+//   return Object.assign({}, object, { [key]: 'value');
+// };
