3cc9d6733230d37ff936e1fef2d7a1635c12eedc
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-09 12:13:45 +0000

    Done.

--- objects.js
+++ objects.js
@@ -105,3 +105,63 @@ recipe // { eggs: 3 }
 function updateObjectWithObject(targetObject, updatesObject) {
   return Object.assign({}, targetObject, updatesObject)
 }
+
+// Deleting a Key-Value Pair
+var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
+ 
+// the `delete` operator returns `true` if it has successfully
+// deleted, `false` otherwise
+delete meals.dinner; // true
+
+meals;
+// returns { breakfast: "oatmeal", lunch: "turkey sandwich" }
+
+// Changing a Value
+var meals = {
+  breakfast: "oatmeal",
+  lunch: "turkey sandwich",
+  dinner: "steak and potatoes"
+};
+meals.breakfast = ["oatmeal", "banana"];
+
+meals;
+// {
+//   breakfast: ["oatmeal", "banana"],
+//   lunch: "turkey sandwich",
+//   dinner: "steak and potatoes"
+//  }
+
+// non-destructively
+var meals = {
+  breakfast: "oatmeal",
+  lunch: "turkey sandwich",
+  dinner: "steak and potatoes"
+};
+ 
+ 
+Object.assign({}, meals, { breakfast: ['oatmeal', 'banana'] })
+// returns {
+//   breakfast: ["oatmeal", "banana"],
+//   lunch: "turkey sandwich",
+//   dinner: "steak and potatoes"
+//  }
+ 
+ 
+meals
+// still {
+//   breakfast: "oatmeal",
+//   lunch: "turkey sandwich",
+//   dinner: "steak and potatoes"
+// };
+
+/*
+Instructions
+Open objects.js
+
+Assign an object to the variable playlist and initialize the object with a key-value pair — the keys will be artist names and the values will be song titles. (What limitation does this impose on our playlist?)
+
+Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
+
+Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
+ */
+ 
