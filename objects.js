d7d571e7d8c18b88f1c464a3fa99e0470ebedff3
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-09 10:34:51 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,3 +6,14 @@ var meals = {};
 // 2nd object
 var meals = new newMeal();
 
+// 3rd object
+var meals = { breakfast: "oatmeal" };
+// or
+var meals = new newMeal({ breakfast: 'oatmeal' })
+
+// duplicate keys are caught as a Warning by the editor!
+var meals = {
+  breakfast: 'eggs',
+  breakfast: 'bacon'
+}
+console.log(meals)
