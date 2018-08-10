3f990d1763102b32c77ff922b050660a396f074e
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-10 02:38:13 +0000

    Done.

--- objects.js
+++ objects.js
@@ -11,11 +11,16 @@ Create a function updatePlaylist that accepts three parameters: the playlist (an
 Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
  */
  
+ 
+ var artistName1 = "My Bloody Valentine"
+ var artistName2 = "Phil Ochs"
+ var artistName3 = "Slowdive"
  var playlist = {
    "My Bloody Valentine": "songTitle1",
    "Phil Ochs": "songTitle2",
    "Slowdive": "songTitle3",
  }
+ 
  
  function updatePlaylist(playlist, artistName, songTitle) {
    /*
