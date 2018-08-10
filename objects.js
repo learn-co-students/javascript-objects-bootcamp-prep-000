ba95492336654addd03fe1a2817b1b10e7a0ef30
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-10 11:29:38 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+ var playlist = {evan:"bring me to life"};
+function updatePlaylist(playlist,artistName,songTitle)
+{
+   playlist [artistName]=  songTitle ;
+}
+function removeFromPlaylist(playlist,artistName)
+{
+  return delete playlist.Slowdive;
+}
