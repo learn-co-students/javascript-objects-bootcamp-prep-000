bfadd90ab376bea6d40e7b823f9c53ec97bb431d
Austin Luft <ausluft@gmail.com>
2018-08-11 15:10:03 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,13 +1,11 @@
-var playlist = { artistName: "song" }
+var playlist = new Object({ artistName: "song" })
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
-  
+  playlist[artistName] = songTitle
   return playlist
 }
 
 function removeFromPlaylist (playlist, artistName) {
-  delete playlist.artistName
-  
+  delete playlist[artistName]
   return playlist
 }
