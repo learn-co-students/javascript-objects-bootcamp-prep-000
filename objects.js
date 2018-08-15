f49f9e6d95af08aedd8127ad1414c6903bd2e116
Roundabout Rider <ntm.email@icloud.com>
2018-08-15 14:07:32 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,13 @@
-var playlists = { artist: `song` }
+var playlist = { artistName: `song`}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+   playlist.artistName = `songTitle`;
+ return playlist
+  
+}
+
+ function removeFromPlaylist (playlist, artistName, songTitle) {
+   delete playlist[artistName];
+ return playlist
+   
+ }
