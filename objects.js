05eeb8e7981a2caa36f0ea0941a75638a3a45749
Violet Moon <violet.wmoon@gmail.com>
2018-08-17 01:17:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  electricLightOrchestra: 'Mr. Blue Sky',
+  pinkFloyd: 'Time'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
