90caeaade6a7e873f3bb2ae2d7cc7d3f8402eece
Angela Lu <angela.lu0919@gmail.com>
2018-08-18 03:13:31 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({'Phil Ochs':'song'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, {artistName:[songTitle]})
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive
+  return playlist
+}
