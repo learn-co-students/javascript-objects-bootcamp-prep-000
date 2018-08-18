1ac83d770b0501d291bc1ebdd95ed5e3d8b8aaa7
Angela Lu <angela.lu0919@gmail.com>
2018-08-18 03:15:43 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.Slowdive
+  delete playlist[artistName]
   return playlist
 }
