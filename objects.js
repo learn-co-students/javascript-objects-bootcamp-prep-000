57b2c62bb0d2b5a23cf79b73a9af92ce8075cb97
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 20:40:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { 'EveryTime I Die' : 'New Black'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+ }
+ 
+ function removeFromPlaylist(playlist, artistName) {
+   delete playlist[artistName];
+ }
