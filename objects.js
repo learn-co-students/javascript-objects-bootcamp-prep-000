a29a1e31a453f9a4d1011783966d3f1a98a15c30
Andrew <andrew.shillito88@gmail.com>
2018-08-22 12:55:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {"Ed Sheeran": "Prefect"};
+
+function updatePlaylist(obj, name, song){
+  obj[name]=song;
+  return obj;
+}
+
+function removeFromPlaylist(obj, name){
+  delete obj[name];
+  return obj;
+}
+
