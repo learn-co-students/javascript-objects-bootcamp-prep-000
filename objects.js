3733088d0130aff3a8550c88aec9fb21340fa444
gc6123@bard.edu <gc6123@bard.edu>
2018-08-23 22:31:42 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = new Object(
+  {diplo: 'boy'}
+  )
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Phil Ochs'] = 'My Bloody Valentine' }
+  function removeFromPlaylist(playlist, artistName) {
+    delete
+    playlist.Slowdive;
+    return playlist
+  }
