ef7eb6d79df958feb400faa66356f1b205402b4a
Sarah Pai <pai.sarah89@gmail.com>
2018-08-24 21:42:54 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,22 @@
 var playlist = {};
+var playlist = {'artist': 'song'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+return Object.assign(playlist, {['Phil Ochs']: "Here's to the State of Mississipi"})
+}
+
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist['Slowdive'];
+return Object.assign({}, {[playlist]: artistName})
+}
+
+
+//--------------------------- below is 1st attempt
+
+
+var playlist = {};
+
 var playlist = { 'artistName': 'songTitle' };
 
 function updatePlaylist (playlist, artistName, songTitle) {
