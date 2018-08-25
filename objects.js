67f3a686925a8c5e7a3272e66ab0946666de5906
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-25 03:43:54 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,12 @@
-var playlist = { 'artistName': 'songTitle' };
+var playlist = { 
+  'artistName': 'songTitle',
+};
 
-function updatePlaylist(playlist, [artistName], songTitle) {
-  Object.assign({}, playlist, {[artistName]: songTitle})
-  return playlist;
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle }) 
 }
 
-function removeFromPlaylist (playlist, artistName) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
