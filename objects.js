d1dcc54d564527eca3a0d0033f8cf4b3dd759a0e
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-26 14:01:31 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,13 @@
-var meals = new
-Object({breakfast: "oatmeal"})
+var playlist = {
+  Beyonce: "Till the end of the time"
+};
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
