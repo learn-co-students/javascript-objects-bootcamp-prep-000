c8b4521b6baf280a7d9d78c7e856447f7f3b2f0c
viqi zevic <vicky.tanzil@gmail.com>
2018-08-30 21:46:39 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+
+var playlist = { playlist: { BSB: 'Everybody' }};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
