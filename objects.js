f521a9664bea54b3cb40fd20753beb0e0d49838e
Alisa McCormick <muntananuchat@gmail.com>
2018-08-31 19:59:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artistName: "songTitle"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist["Phil Ochs"] = "Slowdive"
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive
+  return playlist
+}
