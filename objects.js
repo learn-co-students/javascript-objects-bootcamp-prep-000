cd95f1a1b5c7d9977e3d413218f0c37a107dae9c
Jordan Williams <jordan@jordanchude.com>
2018-09-01 21:52:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { 
+  Kanye: 'Diamonds'
+}
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign(playlist, {[artistName]: songTitle })
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
