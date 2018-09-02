8250b9b22f47577ded101bdc7dbf4c427771fc14
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-02 18:09:01 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,13 @@
-var playlist = {}
+var playlist = {artistName:songTitle}
+var artistName;
+var songTitle;
 
-function updatePlaylist(playlist, artist, song){
-  playlist[(artist)] =[song];
-  playlist;
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
 }
