aab1d07fe3659486ecc85e216391ab5980e8682d
Matthew Dizon <matthewdizon94@gmail.com>
2018-09-03 17:45:53 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'Eminem': 'The Ringer'};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
