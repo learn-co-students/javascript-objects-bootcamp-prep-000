c5238d9ee8c2fc460c13052fc3cccf0519e3fd29
Jae Park <wjp241@nyu.edu>
2018-09-08 13:30:49 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {usher: "My Boo"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+   playlist[artistName] = songTitle;
+   return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
