abdc629526ccb70276ad1ba98473fe84e8d32cac
Nicholas Werner <micropacman@gmail.com>
2018-09-11 20:27:04 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {Thrice: `Beggars`}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
