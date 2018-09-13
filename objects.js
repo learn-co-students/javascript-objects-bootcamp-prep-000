333a0c446e62f68457a8c4256a232f8ba637ade4
matthew.serre@gmail.com <matthew.serre@gmail.com>
2018-09-13 00:49:48 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {Gorillaz: "Saturnz Barz"};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+ playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist[artistName];
+}
