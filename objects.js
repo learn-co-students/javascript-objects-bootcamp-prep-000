1b181d5b0dd3ec37d4b3b400ce7ff454c6e976f9
Matthew Sherman <msherman0801@gmail.com>
2018-09-13 23:47:12 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+
+var playlist = {
+  occupation: 'song',
+  test: 'test'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign(playlist, {[artistName]: songTitle})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+   delete playlist[artistName]
+}
