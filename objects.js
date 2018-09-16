0978b1101ce012b3180317114736d89fa8ac04bd
Joe Huntington <jhuntington@tutanota.com>
2018-09-16 19:06:21 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artist: 'song'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
