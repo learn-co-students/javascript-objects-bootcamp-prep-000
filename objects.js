ff4e3743758c09d4ab014fbda07423695d2f78f9
Virginia Dooley <vcdooley@gmail.com>
2018-09-17 13:26:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {};
+
+var playlist = { artist: "name", song: "title" };
+
+function updatePlaylist(playlist, artist, song) {
+  playlist [artist] = "bruce springsteen",
+  playlist [song] = "thunder road"
+  return playlist
+} 
+
+function removeFromPlaylist(playlist, artist){
+delete playlist.Slowdive;
+playlist;
+}
