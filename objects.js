445f70ce6be17ec3c84aaaa69ef9e9b6d1670608
Brian <briandsalemi@gmail.com>
2018-09-20 01:26:26 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   
   return playlist;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
