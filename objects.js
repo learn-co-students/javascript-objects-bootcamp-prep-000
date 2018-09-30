df77b177e96c48b75b5ee4d6a4cab19bffe4bdcd
Mariel Westervelt <elle.westervelt@gmail.com>
2018-09-30 20:19:42 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Prince: "purple rain"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+    return playlist
+  }
