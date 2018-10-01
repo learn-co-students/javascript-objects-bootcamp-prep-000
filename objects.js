b1b14f58efcf6e0000f56afe746c10d3f69ccba3
Lucy Suddenly <suddenlylucy@gmail.com>
2018-10-01 19:24:32 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {artist: "title"};
+
+function updatePlaylist(playlist,artist,title){
+  playlist[artist] = title;
+  return playlist;
+}
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName];
+  return playlist;
+}
