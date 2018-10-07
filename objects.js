ef83b08ebd269b1bdd859fe34b45b3c93a5bbcc5
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 12:01:19 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = { eminem : "The Ringer"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName]=songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
+
+
