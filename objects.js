5893256a477f01c5a2a2c8533abf0525b75209f7
witty-collection-3981 <yisroel96@gmail.com>
2018-10-08 14:44:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,17 +1,17 @@
 var playlist = {
   'Billy Joel': 'PianoMan',
   Sinatra: 'New york'
-}
+};
 
 function updatePlaylist(playlist, artist, song) {
 
   playlist[artist] = song
 
   return playlist
-}
+};
 
 function removeFromPlaylist(playlist, artist) {
   delete playlist[artist]
 
   return playlist
-}
+};
