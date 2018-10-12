3c66ba60ab13bd8739c25bbdf065a91c3c1ab690
Jonathan Koss <jonathan_koss@yahoo.com>
2018-10-12 22:21:17 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist = { 
-  'Bob Marley': 'Three Little Birds'
+var playlist = {
+  'Michael Jackson': "Beat It"
 };
 
 function updatePlaylist(playlist, artist, song) {
@@ -8,19 +8,5 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artist) {
-  return delete playlist[artist];
+  delete playlist[artist];  
 }
-
-
-/*  
-describe('removeFromPlaylist(playlist, artistName)', () => {
-    it('removes `artistName` from `playlist`', () => {
-      removeFromPlaylist(playlist, 'Slowdive')
-
-      expect(playlist).
-        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-
-      expect(playlist).
-        not.to.have.all.keys({'Slowdive': 'Alison'})
-    })
-  */
