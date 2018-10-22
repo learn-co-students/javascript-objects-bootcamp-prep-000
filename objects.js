b756ecc707534fdcc1a59a128945a68bdf21a767
Jonathan Koss <jonathan_koss@yahoo.com>
2018-10-22 23:47:41 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,20 +1,48 @@
 var playlist = {
-  "Nirvana": "Smells Like Teen Spirit",
-  "Michael Jackson": "Beat It"
+  'Bob Marley': 'One Love',
+  'John Lennon': 'Imagine'
 };
 
-function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song;
-  return playlist;
+function updatePlaylist(object, artist, song) {
+  object[artist] = song;
+  return object;
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist[artist];
- 
+function removeFromPlaylist(object, artist) {
+  return delete object[artist];
 }
 
 /*
-describe('removeFromPlaylist(playlist, artistName)', () => {
+describe('objects', () => {
+  const babelResult = babel.transformFileSync(
+    path.resolve(__dirname, '..', 'objects.js'), {
+      presets: ['es2015']
+    }
+  )
+  jsdom({
+    src: babelResult.code
+  })
+
+  it('defines an object called `playlist` containing at least one artist-song pair', () => {
+    expect(typeof playlist).to.equal('object')
+    expect(Object.keys(playlist).length).to.be.greaterThan(0)
+  })
+
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+
+  describe('removeFromPlaylist(playlist, artistName)', () => {
     it('removes `artistName` from `playlist`', () => {
       removeFromPlaylist(playlist, 'Slowdive')
 
