4cdf1d0c4634f47d5fc3c3359ea900c36455ea85
Lisa Mac <lisamac421@gmail.com>
2018-10-23 19:55:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Madonna:"Papa don't preach"}
+
+function updatePlaylist(playlist, artistName, song){
+  playlist[artistName] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
