cc25179e9ad800059e1468608d3135498da641a9
Rhuwell Samano <rhuwell.samano@gmail.com>
2018-10-29 08:51:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,20 @@
+var playlist = {
+    Drake: "One Dance"
+}
+
+// var playlist = {
+//   'Foo Fighters': 'Everlong',
+//   Prince: 'Purple Rain'
+// }
+
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
+
+  return playlist
+}
