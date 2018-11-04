37510997f1c76437705117c2acaadfe3d4946397
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-04 03:17:01 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,17 +1,28 @@
 var playlist = {
-  'Bob Marley': 'One Love',
+  "Bob Marley": "One Love"
 };
 
-function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song;
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, artist) {
-  delete playlist[artist];
-}
 
 /*
+  it('defines an object called `playlist` containing at least one artist-song pair', () => {
+    expect(typeof playlist).to.equal('object')
+    expect(Object.keys(playlist).length).to.be.greaterThan(0)
+  })
+
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+
   describe('removeFromPlaylist(playlist, artistName)', () => {
     it('removes `artistName` from `playlist`', () => {
       removeFromPlaylist(playlist, 'Slowdive')
