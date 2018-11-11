977afb0aece4a3062dcc273321108c5a0755da83
Dmitriy Sgibnev <dmitriy.sgibnev@gmail.com>
2018-11-11 07:58:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object({ enigma: "Return to Innosense", sod: "Toxicity" })
+var playlist = new Object({ enigma: "Return to Innocence", sod: "Toxicity" })
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle })
