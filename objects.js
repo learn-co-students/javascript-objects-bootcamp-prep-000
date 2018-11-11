18513011883e1aa7a9a995d04a5b1fec07aba9cb
Chris Ball <chrisball7@gmail.com>
2018-11-11 00:18:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = { artist: 'song' }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+
+  return playlist
+}
