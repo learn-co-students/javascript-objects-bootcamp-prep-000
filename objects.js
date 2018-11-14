133129fb990f7dbc70412d0edf59a5a294630f35
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 21:05:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {'The Heavy': 'How Do You Like Me Now?'}
+
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
