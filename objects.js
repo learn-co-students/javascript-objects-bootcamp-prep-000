36407ed19ce8690729d368cdc67f51b680900db2
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-14 19:57:12 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
