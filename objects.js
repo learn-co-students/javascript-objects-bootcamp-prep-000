796799ac23a930fecf640647233746a7dcecbec5
Eric Hardiman <hardimaneric@gmail.com>
2018-11-23 14:01:18 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
-var playlist = { artist: "song" };
+var playlist = { 'Muse': 'Absolution', 'Coheed and Cambria': 'Welcome Home' }
 function updatePlaylist(playlist, artistName, songTitle){
-    playlist[artistName] = songTitle
-    return playlist
+  playlist[artistName] = songTitle
+  return playlist
 }
 function removeFromPlaylist(playlist, artistName){
-    delete playlist.Slowdive
-    playlist;
+  delete playlist[artistName]
+  return playlist
 }
