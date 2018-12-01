eed126f934fd743d6879f433ed386b47473b9934
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-01 03:25:40 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,17 +1,37 @@
 var playlist = {
-  'Bob Marley': 'Three Little Birds'
+  "Bob Marley": "One Love"
 };
 
-function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song;
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artist) {
-  return delete playlist[artist];
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist[artistName];
 }
 
 /*
+   it('defines an object called `playlist` containing at least one artist-song pair', () => {
+    expect(typeof playlist).to.equal('object')
+    expect(Object.keys(playlist).length).to.be.greaterThan(0)
+  })
+
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+
   describe('removeFromPlaylist(playlist, artistName)', () => {
     it('removes `artistName` from `playlist`', () => {
       removeFromPlaylist(playlist, 'Slowdive')
