74f71415d6381ced0c018f812848ef31f35fe8c4
Paris Dolfman <parisdolfman@gmail.com>
2018-12-04 20:26:20 +0000

    Done.

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
    playlist[artistName] = songTitle
    return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
