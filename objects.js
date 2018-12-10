9dda72c516e6be024e80f100623a15f02931d24e
Julian Brown <julianxbrown@gmail.com>
2018-12-10 00:15:42 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+/*global describe, it */
+
+ var playlist = {jayz: 'deadPresidents'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+
+  return playlist;
+}
+
+
+function removeFromPlaylist(playlist, artistName) {
+  var result = delete playlist[artistName];
+
+  return playlist;
+}
+
