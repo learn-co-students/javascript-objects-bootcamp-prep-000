ba945ca45a28482d06430b911e58ee22fc63bbd1
Thomas Hansen <ctrhansen@yahoo.com>
2018-12-11 16:45:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {"Slowdive": 'Alison', "My Bloody Valentine": 'Sometimes', "Phil Ochs": "Here's to the State of Mississippi"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  }
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Slowdive
+}
+
