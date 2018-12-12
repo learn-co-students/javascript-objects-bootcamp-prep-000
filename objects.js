9399a79e7d62a2601be571bead34aaede51b82d4
Charles Treloar <cwtreloar@gmail.com>
2018-12-12 16:09:26 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({ artist: 'song' });
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
