f99e961b2289b2ae87ab1ceb9462dadd79f48370
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-13 18:28:41 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,8 +2,14 @@ var playlist = {
   "Bob Marley": "One Love"
 };
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
 
-
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist[artistName];
+}
 
 
 
