7508aee62b46389834fca276bddff65f48e59e85
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 05:08:50 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {madonna: "borderline"};
+
+function updatePlaylist(playlist, artist, songTitle){
+  playlist[artist] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
