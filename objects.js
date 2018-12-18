097edd5e2bc3ec9ad15cd2d1f7ba566b7e57d68a
Reid Boudreau <rtboudreau@gmail.com>
2018-12-18 05:41:00 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
-var playlist = new Object({keys: '0'});
+var playlist = {
+  'The Roots': 'You Got Me',
+  'Animal Collective': 'Man of Oil'
+}
 
 function updatePlaylist(playlist, artistName, song) {
   playlist[artistName] = song
@@ -9,3 +12,5 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
 }
+
+
