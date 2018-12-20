c9369698a4cb8704069a29e4dfe50236564f9e59
Josephine Yao <yao.josephine@yahoo.com>
2018-12-20 22:07:09 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,21 @@
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"}
+
+  function updatePlaylist (playlist, artistName, songTitle) {
+  return playlist;
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.Slowdive;
+  return playlist;
+}
+  
+  /*function updatePlaylist(playlist, artistName, songTitle) {
+   playlist[artistName] = songTitle
+   return playlist
+ }
+ 
+  function removeFromPlaylist(playlist, artistName) {
+   delete playlist[artistName]
+   return playlist
+ }*/
+
