af0b2124f462bfc4a1553228d3113717b409ef31
Marc <marc.troughton@gmail.com>
2018-12-23 18:01:01 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,10 +1,14 @@
 var playlist = {
   "My Bloody Valentine": "California Love",
+  "Phil Ochs": "No idea"
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
- 
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
- 
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
 
