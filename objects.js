7a5461f3202b16ae0bbb6d03bbb850b60ae5c865
Jaclyn Setina <jaclyn.setina@gmail.com>
2018-12-24 00:09:00 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
 var playlist = new Object({artistName: 'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
   
 }
