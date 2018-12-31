84ed20d1d47c18d73fb6c941e95dcf288806b5fe
Errol W <ewatsonrei@gmail.com>
2018-12-31 21:06:34 +0000

    Done.

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete ({'artistName':);
+  delete playlist["artistName"];
   return playlist;
 }
 
