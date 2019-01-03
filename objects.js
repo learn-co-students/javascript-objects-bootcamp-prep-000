30e52d876ffc97e70b1ff2f40ab0f974521c9d62
Errol W <ewatsonrei@gmail.com>
2019-01-03 23:04:28 +0000

    Done.

--- objects.js
+++ objects.js
@@ -9,10 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist["artistName"];
+  delete playlist[artistName];
   return playlist;
 }
-
-
-
-/* Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist. */
