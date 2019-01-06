093293d04b1478ff84b5e8299a1ad28175a3cbcd
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-06 18:54:17 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {"Slowdive": "Alison", }
+
+function updatePlaylist(playlist, artistName, songTitle)
+ {
+  return playlist["Phil Ochs"] = "Here's to the State of Mississippi"
+ }
+ 
+function removeFromPlaylist(playlist, artistName)
+  {
+   delete playlist.Slowdive
+  }
+  
+  
