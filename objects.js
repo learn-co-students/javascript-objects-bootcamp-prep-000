a5982018cd3b3124e0f269b39927abd7df959d05
Brittany Malooly <bmalooly@gmail.com>
2019-01-07 03:29:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist =  {
+  'Slowdive': 'Alison',
+  'My Bloody Valentine': 'Sometimes',
+  'Phil Ochs': "Here's to the State of Mississippi"
+
+}
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle })
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+  }
