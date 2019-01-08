66115d6157b663bf7140a551b6f032ef4a529ca3
Taher elshafie <t_elshafie10@yahoo.co.uk>
2019-01-08 16:55:21 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,11 @@
-var playlist={'michael':'blue','Tito':'One love'};
-function updatePlaylist(playlist,artistName,songTitle)
-{playlist[artistName]=songTitle
-return playlist}
-function removeFromPlaylist(playlist,artistName)
-{delete playlist.songTitle,
-  return playlist;}
+var playlist = {' michael':'blue', 'Tito':'One love'};
+
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName]=songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist.songTitle,
+  return playlist;
+  }
