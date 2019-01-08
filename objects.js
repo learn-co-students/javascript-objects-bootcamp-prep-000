28840f1c05bf334afdc8a10d0252b9f4f1b85b4d
Taher elshafie <t_elshafie10@yahoo.co.uk>
2019-01-08 15:59:46 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,4 @@
 var playlist={'michael':'blue','Tito':'One love'};
+function updatePlaylist(playlist,'artistName','songTitle')
+{var playlist={'michael':'blue','Tito':'One love'};playlist['artistName']='songTitle'
+return playlist}
