0fa54d5e41ac4b4781f4205971c6b5a12e1da52c
Simon Mei <nagaromo@gmail.com>
2019-01-09 23:36:56 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = new Object({'Ariana Grande': 'Thank you, Next'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign(playlist, {[artistName]: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
