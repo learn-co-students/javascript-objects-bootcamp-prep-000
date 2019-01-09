cb3ad03ef4f660d07af33b737e8a6b71e43fabdf
Ammar Ali <funnyguy118@gmail.com>
2019-01-09 22:07:17 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {Thrice : "Digital Sea"};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  return Object.assign( playlist, { [artistName]: songTitle })
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
