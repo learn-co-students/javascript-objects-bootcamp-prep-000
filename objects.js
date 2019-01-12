a919e680ee4a7f79d723023157c2c2d1dd20ec6a
Taher elshafie <t_elshafie10@yahoo.co.uk>
2019-01-12 02:20:44 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName:'blue'};
 
 function updatePlaylist(playlist,artistName,songTitle){
-  playlist.artistName='songTitle'
+  playlist[artistName]=songTitle;
   return playlist
 
 
