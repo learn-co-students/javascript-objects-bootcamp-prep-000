d909888deae9b69fce8aedf7d4ffc7df1ce6c8af
Begum S. Tuyen <begumtuyen@gmail.com>
2019-01-14 15:32:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { Slowdive : "Alison", ['My Bloody Valentine'] : "Sometimes"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Slowdive
+}
