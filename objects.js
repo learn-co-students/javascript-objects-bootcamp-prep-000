b01e53ab467ff65def52e88770281c8a4bf742e3
Sean Carter <scarter0313@gmail.com>
2019-01-20 02:45:43 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
-  artistName: ["Slowdive", "My Bloody Valentine"]
-  songTitle: ["Alison", "Sometimes"]
+  "Slowdive": "Alison",
+   "My Bloody Valentine":  "Sometimes"
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
