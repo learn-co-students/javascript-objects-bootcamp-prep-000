c695592ebb9f895020407ab46795d4c4c9f49e38
Ryan Jasinski <jasinski.17@gmail.com>
2019-01-22 15:57:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  'Bush': 'Song'
+};
+
+function updatePlaylist (obj, artist, song) {
+  obj[artist] = song;
+  return obj;
+}
+
+function removeFromPlaylist (obj, artist) {
+  delete obj[artist];
+  return obj;
+}
