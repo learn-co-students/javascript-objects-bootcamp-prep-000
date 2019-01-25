791365a60d293ef6567f376b1d640611102de057
Harry Bigos <hrbigos@gmail.com>
2019-01-25 21:10:06 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,14 +1,10 @@
-var playlist = new Object({ Epik_High: "Born Hater"});
-
+var playlist = new Object({ Ariana_Grande: "Thank You, Next" });
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
 }
-
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
 }
-
