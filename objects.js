31f08f4f0df2bb735c32cca2c637c59b31e6dbc6
Karyna Dominguez <karinaminty@gmail.com>
2019-01-29 04:42:16 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist={johnLenon: "Jealous guy", BillyJoe: "Holiday"}
+
+function updatePlaylist (playlist, artistName, songTitle){
+ playlist[artistName]=songTitle
+ return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+  
+}
