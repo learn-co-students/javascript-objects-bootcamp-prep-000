a9687c35f8769b6d3baa9cd630e5e37be9ee354a
Alexandre Silva Dias <alexdias53@gmail.com>
2019-02-07 15:28:21 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist = { artistName: "songTitle"
+  
+};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  
+  playlist[artistName] = songTitle
+  
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  
+  delete playlist [artistName]
+  
+  return playlist
+}
