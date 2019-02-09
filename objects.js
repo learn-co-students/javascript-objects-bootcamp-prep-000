9097e0cfa162a6acba106687843143f6b7a7dc20
casey bradford <casey@flatironschool.com>
2019-02-09 17:54:07 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { artist: "title"};
+
+function updatePlaylist(obj, key, value){
+  obj[key] = value
+  
+  return obj
+}
+
+function removeFromPlaylist(obj, key){
+  delete obj[key];
+  return obj;
+}
