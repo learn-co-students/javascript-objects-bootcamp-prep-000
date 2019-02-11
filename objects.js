21df4248d2c18e73f8e9340329a3444ff30e93a3
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-11 11:33:55 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+
+
+var playlist = {artistName: 'songTitle'};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist [artistName] = 'songTitle';
+
+}
+
+function removeFromPlaylist (playlist,artistName){
+  delete playlist ['Slowdive'];
+}
