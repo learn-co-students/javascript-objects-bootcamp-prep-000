1ec213a102de05b390e1ed91181c6cfe51e240e5
Patrick Lebsack <p_lebbysack@hotmail.com>
2019-02-12 22:06:48 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  'Nickleback': "Garbage",
+  'Prince': "Purple Rain",
+}
+
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
