dbad0caaa6678e6ad3d7dca5d7509fb4c518d2dd
Christopher Cabell <cccabell@icloud.com>
2019-02-25 23:18:08 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {BRMC: "Talk"}
+
+function updatePlaylist(obj, artist, song) {
+  return obj[artist] = song
+}
+
+function removeFromPlaylist(obj, artist) {
+  delete obj[artist]
+  return obj
+}
