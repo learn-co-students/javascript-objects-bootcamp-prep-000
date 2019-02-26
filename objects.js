894938742d5eb7f8fe89ec273b81079b90916b2f
Meet Shah <meetdshah007@gmail.com>
2019-02-26 10:04:58 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+let playlist = {
+  song: "abc"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
