b2db859a3917f40eed201544553d03a3c07bb7b0
Erini George <dr.erinigeorge@yahoo.com>
2019-02-27 00:57:18 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = { a : "b", c : "d", e:"f"};
+
+function updatePlaylist ( obj , key, value)
+{
+  obj[key] = value;
+  return obj;
+}
+
+function removeFromPlaylist (obj, key)
+{
+  delete obj[key];
+  return obj;
+}
