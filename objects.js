787d5d4189073e5c398cad202eeb46add297c3e3
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 15:05:07 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {artist: "song"}
+
+function updatePlaylist(playlist,artistName,songTitle){
+playlist[artistName] = songTitle
+}
+function removeFromPlaylist(playlist, artistName){
+delete playlist[artistName]
+
+}
