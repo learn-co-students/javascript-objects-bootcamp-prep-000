cc4ccd2a5f9f0350b86f0d493eedadbf963ef98a
Will Smith <smithw1994@gmail.com>
2019-03-03 04:28:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {a: 'b'}
+
+function updatePlaylist (playlist, artist, title) {
+  playlist[artist] = title
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
