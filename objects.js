248bead11eb2a4d803161a9083b0e50293f54be6
Annia Forney <anniaforney@gmail.com>
2019-03-06 00:24:00 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,20 @@
+var playlist {
+ artist name: 'Lady Gaga',
+ song title: 'Shallow'
+};
+
+var updatePlaylist{
+  playlist: 'Indie'
+  artist name:'Tame Impala'
+  song title: 'Keep On Lying'
+};
+
+var removeFromPlaylist{
+  delete.var 'Lady Gaga';
+  playlist;
+}
+
+var removeFromPlaylist{
+  delete.var 'Shallow'
+  playlist;
+}
