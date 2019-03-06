e33c0172eafe58d82392594e3df5a4e415e73b04
Annia Forney <anniaforney@gmail.com>
2019-03-06 00:36:53 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,20 +1,27 @@
-var playlist {
- artist name: 'Lady Gaga',
- song title: 'Shallow'
+var playlist ;{
+ playlist: ['Shallow'] ['Lady Gaga']
+ playlist: ['Ring'] 'Cardi B'
 };
 
-var updatePlaylist{
-  playlist: 'Indie'
-  artist name:'Tame Impala'
-  song title: 'Keep On Lying'
-};
+var updatePlaylist ;{
+  playlist: ['Indie'],
+  artist name:['Tame Impala'],
+  song title: ['Keep On Lying']
+}
 
-var removeFromPlaylist{
-  delete.var 'Lady Gaga';
+var removeFromPlaylist ;{
+  delete.var playlist;
   playlist;
 }
 
-var removeFromPlaylist{
-  delete.var 'Shallow'
-  playlist;
+var updatePlaylist ;{
+  playlist: ['Indie'],
+  artist name:['Tame Impala'],
+  song title: ['Keep On Lying']
 }
+
+Object.assign({}, playlist, {'Indie','Tame Impala','Keep On Lying' })
+
+
+
+playlist
