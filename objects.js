b9f9065f2cc03b8ff4e1e0dcb517bbea6d85e462
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 16:28:48 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { 'Yvette Young': "Mermaid"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
