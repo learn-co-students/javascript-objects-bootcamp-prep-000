4b99d9ffda5255626abd783c2a3d2a2698c668c8
Christopher Yuan <chrisyuan7@gmail.com>
2019-03-26 19:12:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Nas: "Rewind"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
