efead3264dd0e90d1eac2d419b8dfedf59f46a22
Sean Barile <aquariussb84@gmail.com>
2019-04-01 00:52:11 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,10 @@
-var playlist = ({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
-  return playlist
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return playlist;
-}
+ var playlist = ({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+ 
+ function updatePlaylist(playlist, artistName, songTitle) {
+   playlist.artistName = songTitle
+   return playlist
+ }
+ 
+ function removeFromPlaylist(playlist, artistName) {
+   delete playlist [artistName]
+ }
