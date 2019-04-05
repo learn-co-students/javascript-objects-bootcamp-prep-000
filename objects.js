735836dfa653478b37169cba189ad3e040054d63
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-05 22:35:18 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
-let playlist = { hendrix: "Purple Haze"}
+let playlist = { Hendrix: "Purple Haze"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  
+  delete playlist.Slowdive
+  return playlist
 }
