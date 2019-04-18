c6fcfdad9658c18a294c769006eb9fe10022bfd1
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-18 02:16:16 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,12 +1,1 @@
-let playlist = {
-  'Bob Marley': 'Three Little Birds' 
-};
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle;
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  return delete playlist[artistName];
-}
