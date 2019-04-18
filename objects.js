d7de156ab27b70cb5e6fe05a5a90c277499bbd21
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-18 02:32:09 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,13 @@
+let playlist = {
+  'Rolling Stones': 'Sympathy for the Devil'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist[artistName];
+}
 
