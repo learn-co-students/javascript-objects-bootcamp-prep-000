430fb6e68637fc239d7a2e9a6083b9703b6c811d
Danielle Martinoli <daniechris5@gmail.com>
2019-04-21 15:41:49 +0000

    Done.

--- objects.js
+++ objects.js
@@ -3,11 +3,11 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    playlist.artistName = songTitle
+    playlist[artistName] = songTitle
     return playlist
 }
 
 function removeFromPlaylist(playlist, artistName, songTitle){
-    delete playlist.artistName;
+    delete playlist[artistName];
     return playlist
 }
