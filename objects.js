60405dce1d52807e29cce5b0a9e5d3d763a85d3a
Alexander Schelchere <ascellie@gmail.com>
2019-04-24 20:22:59 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,4 +1,10 @@
-let [ariana, bowie, lilN, jCole, travis, lilD, drizzy, cal, day, chris, eagles] = ['Ariana Grande', 'David Bowie','Lil Nas X', 'J. Cole', 'Travis Scott', 'Lil Dicky', 'Drake', 'Calvin Harris', 'Dan + Shay', 'Chris Stapleton', 'Eagles'];
+let [ariana, bowie, lilN, 
+jCole, travis, lilD, 
+drizzy, cal, day, 
+chris, eagles] = ['Ariana Grande', 'David Bowie',
+'Lil Nas X', 'J. Cole', 'Travis Scott', 
+'Lil Dicky', 'Drake', 'Calvin Harris', 
+'Dan + Shay', 'Chris Stapleton', 'Eagles'];
 
 var playlist = {
   lilN : 'Old Town Road',
@@ -26,6 +32,9 @@ function updatePlaylist(myPlaylist, artistName, songTitle) {
   return newPlaylist;
 }
 
-function removeFromPlaylist(myPlaylist) {
+function removeFromPlaylist(myPlaylist, artistName) {
+  
+  delete myPlaylist[artistName];
+  return myPlaylist;
   
 }
