916bddbd37f5db92127175777e054e90c1b52a61
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-04-26 22:13:51 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+//objects defines an object called `playlist` containing at least one artist-song pair:
+let playlist= { Pink: "Hello"};
+
+//objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
+function updatePlaylist (playlist, artistName, songTitle){
+   playlist[artistName]= songTitle;
+return playlist;
+}
+
+// objects removeFromPlaylist(playlist, artistName) removes `artistName` from `playlist`:
+function removeFromPlaylist(playlist, artistName){
+  delete.playlist['artistName'];
+  return playlist;
+}
