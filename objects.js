6736956ae41815d08ad81bc012c6f60283121573
Marco Frisina <marco@frisina.xyz>
2019-05-03 15:03:52 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Tupac: "Me against the world"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+playlist[artistName] = songTitle;
+return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
