df8edbf85058867a86e7ce26ce2112cafe69e86a
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-06 01:50:41 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,3 +1,12 @@
 let playlist = {
   "Bod Dylan": "Like a Rolling Stone"
 };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist[artistName];
+}
