ec895ba08d3ff4b474030d96168ae1d667c3a5f9
William Chang <wdchang86@gmail.com>
2019-05-07 19:47:30 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'Coldplay': 'A Message'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
