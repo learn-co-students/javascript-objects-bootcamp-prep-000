843692c243a5c449712e2148c37eb2b6ab74a426
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-12 13:37:36 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {artistName: 'song'};
+
+function updatePlaylist (playlist, artistName, songTitle){
+  playlist[artistName]=songTitle
+}
+ function removeFromPlaylist(playlist,artistName){ 
+delete playlist[artistName]
+}
