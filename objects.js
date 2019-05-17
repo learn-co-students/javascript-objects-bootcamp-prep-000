9331c520115c21e5c0f885078cd02f284493e9e5
Yooni Park <yoonikj@gmail.com>
2019-05-17 04:30:37 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  'BTS': 'Spring Day'
+};
+
+function updatePlaylist(playlist, artist, title) {
+  playlist[artist] = title;
+  
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  
+  return playlist;
+}
