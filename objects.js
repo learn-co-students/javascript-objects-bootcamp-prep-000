07e8d57c6087768118451af9115b00492e4e08da
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-17 18:20:53 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {'Foo Fighters': "Pretender"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['Slowdive'] = 'Alison'
+  playlist['My Bloody Valentine'] = 'Sometimes'
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+  return playlist 
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Slowdive;
+  return playlist
+}
