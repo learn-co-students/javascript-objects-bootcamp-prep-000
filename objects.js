8e9c3989dfcbfceb0db82a3443ffa452ea19227e
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-22 17:41:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {rend: 'joy', culture: 'even so'};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
