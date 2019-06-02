d2debfbebe68db605f58763e7d2fe56e95e8ace3
David Little <littledavid175@gmail.com>
2019-06-02 18:23:16 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist={ 
+  artist: 'ACDC',
+  song: 'thunderstruck'
+}
+function updatePlaylist (playlist,artistName,songTitle) {
+playlist['Phil Ochs'] = "here's to the State of Mississippi"
+  return updatePlaylist
+}
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist["Slowdive"]
+  return removeFromPlaylist
+  
+}
