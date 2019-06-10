749f4521d93787afd5ebcf56f992b290f1cbae34
Carlotta Junin <carlotta.junin@gmail.com>
2019-06-10 19:27:09 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,15 +1,16 @@
-var playlist = {Slowdive: "Alison"};
+var playlist = {tool:"laterlaus"};
 
 function updatePlaylist(playlist, artistName, songTitle){
 
-  return Object.assign({}, playlist, {[artistName]:songTitle})
+  return Object.assign(playlist, {[artistName]:songTitle})
 }
 
 updatePlaylist(playlist, "My Bloody Valentine", "Sometimes");
 
-function removeFromPlaylist(playlist, artistName) {
+function removeFromPlaylist(playlist, artistName){
 
   delete playlist.artistName
+
 }
 
-removeFromPlaylist(playlist, "Slowdive")
+removeFromPlaylist(playlist, tool);
