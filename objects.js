c6ca22fff3becf0475f83c1c84ad93c7a8f27252
Fernanda Romero <fernandaromero85@gmail.com>
2019-06-11 01:42:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  SIA: "chandelier",
+  ColdPlay: "Yellow",
+  Quen: "Radio Gaga"
+};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
