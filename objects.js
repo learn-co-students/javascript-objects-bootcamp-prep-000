118ccfbfcdbb4b784509f4e0ef046d5166fa8b4a
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-25 12:15:35 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,15 +1,11 @@
-var playlist = {
-  artist: 'song pair'
-};
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle;
-  return playlist;
-  // return Object.assign({}, playlist, {[artistName]: songTitle})
+const playlist = {'Talal Maddah': 'A7ebak lo'}
+function updatePlaylist(obj, key, value){
+  obj[key]= value;
+  return obj
 }
 
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.Slowdive;
-  // return playlist
+function removeFromPlaylist(obj, key){
+  delete obj[key]
+  return obj;
 }
-
