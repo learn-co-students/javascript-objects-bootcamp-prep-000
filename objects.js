cb9677778fa944fcb207fa2f12b47e8283a3300e
Bello Osagie <osagienoah04@gmail.com>
2019-07-06 00:13:20 +0000

    Done.

--- objects.js
+++ objects.js
@@ -7,8 +7,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
 }
+removeFromPlaylist(playlist, 'Slowdive')
