20a41d40b2672a11505fcd9da68a9ca4e406456b
Bello Osagie <osagienoah04@gmail.com>
2019-07-06 00:02:36 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = {
+  Slowdive: "Alison",
+  myBloodyValentine: "Sometimes"
+};
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
