3b5e683216f077e39b63945c2dce4ec82c3c7d01
Bello Osagie <osagienoah04@gmail.com>
2019-07-06 01:14:01 +0000

    Done.

--- objects.js
+++ objects.js
@@ -19,3 +19,23 @@ removeFromPlaylist(playlist, 'Slowdive')
 /*
 { 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': 'Here\'s to the State of Mississippi' }
 */
+
+/*
+preset: ES2015(ES6)
+const playlist = {
+  "Slowdive": "Alison",
+  "My Bloody Valentine": "Sometimes"
+}
+
+const updatePlaylist = (playlist, artistName, songTitle) => {
+  playlist[artistName] = songTitle
+  return playlist
+}
+console.log(updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi"))
+
+const removeFromPlaylist = (playlist, artistName) => {
+  delete playlist[artistName]
+  return playlist
+}
+console.log(removeFromPlaylist(playlist, 'Slowdive'))
+*/
