de2b70aebe94abf3fa2978422648f17e4ccb2e85
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-09 14:56:47 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'Kanye west': "Devil in a new dress"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist
+}
