430348abffcd1fedef4ed37516c85b43b145ee4d
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-10 22:42:18 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  artistname: 'songtitle'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  
+  playlist[artistName] = 'songTitle'
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+
+}
