5eb6dd677650c968335b027bb5a4c61ebc9f83b2
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-07-16 17:40:24 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  "Phil Ochs": "Mushu"}
+  
+  function updatePlaylist(object, string, name) {
+    playlist.Slowdive = "alison";
+  }
+  
+  function removeFromPlaylist(object, string) {
+    delete playlist.Slowdive;
+  }
