7ef1696ff4d6c495cd9a328606609ff8e765ac9a
Angel Alanis <a4alaniz@gmail.com>
2019-07-20 19:07:58 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {name: "song"};
 playlist['name']
 
-function updatePlaylist(playlist, Eminem, Mockingbird) {
-  playlist[Eminem] = Mockingbird
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
   return playlist
 }
