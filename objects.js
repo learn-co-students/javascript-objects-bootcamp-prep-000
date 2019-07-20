f8ad60697d65e256f508b1b5cd03217d58d8a120
Angel Alanis <a4alaniz@gmail.com>
2019-07-20 19:35:26 +0000

    Done.

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
