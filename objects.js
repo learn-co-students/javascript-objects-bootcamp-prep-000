cc660ae4fd9478f032c5f3149996a106c32858b3
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-24 18:31:05 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,5 +4,6 @@ function updatePlaylist(playlist, artistName, song) {
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
+  return playlist;
 }
