8e45549ca9a36e980f458903c78ed7adf8d7d6a9
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-24 18:28:13 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {"Justin Timberlake": "What goes around"};
+function updatePlaylist(playlist, artistName, song) {
+  playlist[artistName]= song;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
