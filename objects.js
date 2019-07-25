08528bc3c240ba7046c6f445a02f123ab33bd1fe
Shubinder <shubinder92@gmail.com>
2019-07-25 12:09:32 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({50 Cent: "Candy Shop"});
+ 
+
+function  updatePlayList (playlist, "Malkit Singh","Tutak Tutiyan")
+
+
+function removeFromPlaylist (playlist "Malkit Singh")
+
+return 
+
+console.log (playlist)
