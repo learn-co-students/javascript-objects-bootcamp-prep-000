b6234b794f88146bf219f81e3ab5dbee8c6a0241
Barbara Muesing <muesingb@gmail.com>
2019-07-27 18:08:36 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,13 +1,14 @@
 var playlist = {
   BTS: "Boy with Luv(feat.Halsey)",
-  Lizzo: "Truth Hurts"
+  Lizzo: "Truth Hurts",
+  Led_Zeppelin : "Stairway to Heaven"
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
-}
+};
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.songTitle
+  delete playlist[artistName]
   return playlist
-}
+};
