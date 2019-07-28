5541b51f69c0ab16d87e1ef8096e0640c6e075f6
Priscilla Firpo <priscillayf@gmail.com>
2019-07-28 20:55:16 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  "Slowdive": "Alison",
+  "My Bloody Valentine": "Sometimes",
+  "Phil Ochs": "Here's to the State of Mississippi"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = "songTitle"
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive
+  return playlist
+}
