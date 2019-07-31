09e3c6614e7046a28b6a38098cb88d445ea0b7d2
Katy Morton <krokmorton@gmail.com>
2019-07-31 16:53:43 +0000

    Done.

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
- delete playlist.Slowdive
+ delete playlist[artistName]
  return playlist
-}
+}le
