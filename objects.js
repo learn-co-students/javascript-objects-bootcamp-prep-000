08254bc0d8b581aa5b3d659239f2ce1718b84765
Katy Morton <krokmorton@gmail.com>
2019-07-31 16:34:40 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Air: 'Moon Safari'
+}
+
+function updatePlaylist(playlist, artistName, song){
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+ delete playlist.artistName
+ return playlist
+}
