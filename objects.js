9f52e3110029190b1b2fcbd9ab6b012a2e0a1bb2
Jay Wall <jaywall@gmail.com>
2019-08-04 21:51:03 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {artistName: "James Brown", songTitle: "Gotta Get Down"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, { [artistName]: artistName});
+  Object.assign({}, playlist, {[songTitle]: songTitle });
+   return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+delete playlist.artistName
+return playlist
+}
