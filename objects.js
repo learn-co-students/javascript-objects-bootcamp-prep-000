730cbfee425ca4fcca86908ef3afc21ed48b4853
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-10-10 14:43:53 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,17 @@
-var playlist= artist:'song'
+var playlist= {artist:'song' };
+
+/*function updatePlaylist(playlist,artistName,songTitle) { return Object.assign(
+ {},playlist, {artist:['song']})
+} */
+
+function updatePlaylist(playlist,artistName,songTitle){
+  
+  // return playlist [artist:'song'];
+   playlist [artistName]= songTitle;
+   return playlist; 
+
+}
+
+function  removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+}
