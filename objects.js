e26a396b419bdd7f065228accb018d189764f65e
Stephen Crane <scrane88@gmail.com>
2019-11-26 23:03:05 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {"Tipper" : "Cinder Cone"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
