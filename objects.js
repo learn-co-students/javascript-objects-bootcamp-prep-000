e8104e5affb6d9f962cc3059cc6643d9c6a9a2c8
Steven Luck <steve.luck@gmail.com>
2019-12-18 01:22:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'The Band':'The Weight'}
+function
+updatePlaylist (myplaylist,artist,SongTitle) {
+  return myplaylist
+}
+
+function
+removeFromPlaylist (obj,artist) {
+  delete obj.artist
+}
