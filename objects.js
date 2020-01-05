c9deff8e600cc9faec8b5e432a648a7a8c7a1043
qdu628 <qdu628@gmail.com>
2020-01-05 00:54:10 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {artistName: 'songTitle'};
+
+function updatePlaylist(obj, key, value) {
+ 
+  return Object.assign(obj, { [key]: value });
+}
+
+function removeFromPlaylist(obj, key) {
+  delete obj[key];
+  return obj;
+}
