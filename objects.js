645941b3b1149dd3729f6397dc1b63968c0d5e9b
Richard Vernon Dyson <dyson77z@gmail.com>
2020-01-23 03:46:17 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  KanyeWest: "Runaway",
+  GZA: "Liquid Swords",
+  MaxoKream: "Meet Again"
+};
+
+function updatePlaylist(playlist, artistName, songTitle ) {
+ playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName] 
+  return playlist
+}
+
