b1dc10188952afef854449840c6224e69f739cf0
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 10:37:38 +0000

    Done.

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = { ibo : "haydisoyle" }
 function updatePlaylist(playlist, artistName, songTitle)
 {
   playlist[artistName]=songTitle;
-  // Object.assign({},playlist,{[artistName]:songTitle})
   return playlist
 }
-function removeFromPlaylist(playlist, artistName)
+function removeFromPlaylist(playlist,artistName)
 {
-  delete playlist.artistName
+   return delete playlist[artistName];
+  
 }
