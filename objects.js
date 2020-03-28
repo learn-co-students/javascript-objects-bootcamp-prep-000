6e2cecd41760bd89c41e213f13461194a1643da8
zaid85j <zaid85j@gmail.com>
2020-03-28 09:04:49 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 
-function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, { [artistName]: songTitle })
+function updatePlaylist(obj, key, value){
+  return Object.assign({}, obj, { [key]: value })
 }
 var playlist={artistName:"Zaid Habash"}
 playlist={songTitle:"Moon Light"}
