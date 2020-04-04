d0153b82ca5225c312a49c91c343e1c412458677
Jake Gonchar <jake098877@gmail.com>
2020-04-04 01:07:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { artist: "song" };
+function updatePlaylist(playlist, artist, song) {
+playlist[artist] = song;
+return playlist;
+}
+function removeFromPlaylist(playlist, artist) {
+delete playlist.Slowdive;
+return playlist;
+}
