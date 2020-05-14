2d4a38d9e2b5eee45f1cd64a2d119203ed847fdf
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-14 18:47:08 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist={AllisonKrause:"Whiskey Lulliby"}
+
+
+function updatePlaylist(playlist,artist,song){
+  playlist[artist] = song;
+  return playlist
+}
+  
+
+function removeFromPlaylist(playlist,artist){
+  delete playlist[artist]
+  return playlist
+}
+
+
