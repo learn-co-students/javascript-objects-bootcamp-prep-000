8852801bbe2609618182c25930aeca985c80ee12
Nathan Chung <chung.nathanh@gmail.com>
2020-05-27 21:56:27 +0000

    Done.

--- objects.js
+++ objects.js
@@ -4,8 +4,11 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, { 'Bruno Mars': ['Treasure'] })
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+  return playlist
 }
+
