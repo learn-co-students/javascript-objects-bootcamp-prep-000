15bb275e4600186c6fb06630f6c641a80bf4dde3
Tracy Ngo <tngo1206@gmail.com>
2020-06-23 09:35:10 -0700

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {artistName: 'Scandal'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
