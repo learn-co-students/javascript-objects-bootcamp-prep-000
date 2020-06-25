c85ef84d04fb16a0d274e8819e29f3d79aba9fad
Gregory Singleton <yadig69design@gmail.com>
2020-06-25 04:14:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  "pink Flody": "money"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
