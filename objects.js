ac3b21d928042e6049ee32893e2535d6f72d1322
Aurangzaib <aurangzaib.danial@gmail.com>
2020-08-03 12:23:40 +0500

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {kaneyWest: 'I love apples'}
+
+var updatePlaylist = (playlist, artistName, songTitle) => {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+var removeFromPlaylist = (playlist, artistName) => {
+  delete playlist[artistName]
+  return playlist
+}
