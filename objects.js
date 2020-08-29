e4c189a7cac66fed18926f71abce409a832ea557
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-29 01:02:44 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,28 @@
+var playlist = {
+  Slodive: "Alison",
+  myBloodyValentine: "Sometimes",
+ 
+}
+
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  
+  playlist[artistName] = 'songTitle';
+  return(playlist);
+  
+}
+
+
+function removeFromPlaylist(playlist, artistName) {
+  
+  delete playlist[artistName]
+  return(playlist)
+  
+  
+}
+
+
+
+
+
+
