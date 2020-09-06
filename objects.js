eaaebf8e6256c0f421fdd7b6de118451711d92fe
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-06 18:38:48 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist=Object({ACDC:"THUNDERSTRUCK"});
+
+function updatePlaylist(playlist, artistName, songTitle){
+
+
+  playlist[artistName]=songTitle;
+  return playlist;
+}
+  
+  
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  
+  
+}
