da771fd6d0a1f3e2723bda10658062f4583c2faf
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-08 21:41:23 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {KidCudi: 'Day N Night'}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+
+  return playlist
+}
+
+ function removeFromPlaylist(playlist, artistName) {
+   delete playlist[artistName]
+
+   return playlist
+ }
