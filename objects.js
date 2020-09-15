f7aa5c54c4d2b022124dde5678f29b605c20e13b
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 16:26:17 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+'Slowdive': 'Alison', 
+'My Bloody Valentine': 'Sometimes'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist["Phil Ochs"] = "Here's to the State of Mississippi"
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName, songTitle) {
+  delete playlist.Slowdive;
+  return playlist
+}
