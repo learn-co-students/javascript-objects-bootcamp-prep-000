f377696302ae434ab130fe4b5575fc3e6883824d
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-22 08:44:30 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = {artist: "name", song: "name"}
+var playlist = {artistName: "Julia", songTitle: "Why Come Back"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
