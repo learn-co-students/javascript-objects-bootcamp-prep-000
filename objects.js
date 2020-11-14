ac071eee025205448259c986ce54be89978946bf
Amaury Burgos <amauryb.1204@gmail.com>
2020-11-14 17:41:54 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { LilDurk: "Chiraqimony" }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign(playlist, { [artistName]: songTitle})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
