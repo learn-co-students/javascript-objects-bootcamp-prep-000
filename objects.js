cf36a8dd9ce79fbe3b3017c12559ece871a8c0f9
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 23:52:36 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {}
+var playlist = new Object();
+var playlist = {dagar: "yaman"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
